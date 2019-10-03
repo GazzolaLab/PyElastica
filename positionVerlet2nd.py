65bcbb5110ad3d15122c49ac90b663e659bb4867
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-10-03 13:58:41 -0500

    fmt : black rod and positionVerlet

--- positionVerlet2nd.py
+++ positionVerlet2nd.py
@@ -18,9 +18,9 @@ class positionVerlet2nd(object):
     def pass_neumann(some_obj, rhs_linear, rhs_angular, *args, **kwargs):
         return rhs_linear, rhs_angular
 
-    def __init__(self, obj, dirichletBC=pass_dirichlet,
-                 neumannBC=pass_neumann,
-                 *args, **kwargs):
+    def __init__(
+        self, obj, dirichletBC=pass_dirichlet, neumannBC=pass_neumann, *args, **kwargs
+    ):
         self.obj = obj
         self.dt_prev = 1.0
         self.dirichletBC = dirichletBC
@@ -30,7 +30,7 @@ class positionVerlet2nd(object):
         self.__dict__.update(kwargs)
 
     def step(self, dt):
-        '''
+        """
 
         === Phase 1 ===
         (1) r(t + dt/2) = r(t) + dt/2 * v(t)
@@ -58,17 +58,17 @@ class positionVerlet2nd(object):
         (7) r(t + dt) = r(t + dt/2) + dt/2 * v(t + dt)
         (8) Q(t + dt) = exp(dt/2 w(t + dt)) * Q(t + dt/2)
         ===============
-        '''
+        """
 
         """################### Phase 1 ######################"""
-        self.obj.r += dt / 2. * self.obj.v
-        rot = self.obj.rotate_v(dt/2., self.obj.w)
+        self.obj.r += dt / 2.0 * self.obj.v
+        rot = self.obj.rotate_v(dt / 2.0, self.obj.w)
         self.obj.Q = rot @ self.obj.Q
         ###################################################
 
         # Apply BC and update current time
         self.dirichletBC(self.obj, self.time)
-        self.time += dt / 2.
+        self.time += dt / 2.0
 
         """#################### Phase 2 ######################"""
         """##### Linear and angular velocity"""
@@ -77,7 +77,8 @@ class positionVerlet2nd(object):
 
         # forces and torque goes here (ie contact, friction, muscular activity)
         rhs_linear, rhs_angular = self.neumannBC(
-            self.obj, rhs_linear, rhs_angular, self.time)
+            self.obj, rhs_linear, rhs_angular, self.time
+        )
 
         self.obj.v += dt * rhs_linear
         self.obj.w += dt * rhs_angular
@@ -87,12 +88,11 @@ class positionVerlet2nd(object):
         self.dirichletBC(self.obj, self.time)
 
         """#################### Phase 3 ######################"""
-        self.obj.r += dt / 2. * self.obj.v
-        rot = self.obj.rotate_v(dt/2., self.obj.w)
+        self.obj.r += dt / 2.0 * self.obj.v
+        rot = self.obj.rotate_v(dt / 2.0, self.obj.w)
         self.obj.Q = rot @ self.obj.Q
         ###################################################
 
         # Store previous time step and update current time
         self.dt_prev = dt
-        self.time += dt / 2.
-
+        self.time += dt / 2.0
