fix : bug in inverse rodrigues rotation, thanks pytest

elastica/_rotations.py
@@ -352,7 +352,7 @@ def _inv_rotate(director_collection):
     filter_idx = np.where(np.abs(theta_collection) < 1e-14)
 
     # Scale the vector collection by \theta/sin(\theta), from Rodrigues
-    vector_collection *= theta_collection / np.sin(theta_collection)
+    vector_collection *= 0.5 * theta_collection / np.sin(theta_collection)
 
     # Set filter_idx locations to 0.0
     vector_collection[..., filter_idx] = 0.0