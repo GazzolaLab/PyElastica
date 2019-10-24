fix : ambiguity with signs

elastica/_rotations.py
@@ -3,7 +3,7 @@ import functools
 from itertools import combinations
 
 import numpy as np
-
+from ._linalg import _batch_matmul
 
 # TODO Check feasiblity of Quaternions here
 
@@ -297,8 +297,7 @@ def _rotate(director_collection, scale: float, axis_collection):
 
     # TODO Finish documentation
     """
-    return np.einsum(
-        "ijk,jlk->ilk",
+    return _batch_matmul(
         _get_rotation_matrix(scale, axis_collection),
         director_collection,
     )
@@ -326,8 +325,12 @@ def _inv_rotate(director_collection):
     """
 
     # Q_{i+i}Q^T_{i} collection
+    # rotmat_collection = np.einsum(
+    #     "ijk, ljk->ilk", director_collection[:, :, 1:], director_collection[:, :, :-1]
+    # )
+    # Q^T_{i+i}Q_{i} collection
     rotmat_collection = np.einsum(
-        "ijk, ljk->ilk", director_collection[:, :, 1:], director_collection[:, :, :-1]
+        "jik, jlk->ilk", director_collection[:, :, 1:], director_collection[:, :, :-1]
     )
 
     # Returns rate-of-change direction as a collection unit vectors

tests/test_rotations.py
@@ -332,11 +332,12 @@ def test_inv_rotate_correctness_simple_in_three_dimensions():
     # A rotation of 120 degrees about x=y=z gives
     # the permutation matrix P
     # {\begin{bmatrix}0&0&1\\1&0&0\\0&1&0\end{bmatrix}}
-    # Hence if we pass in I and P, the vector returned should be
+    # Hence if we pass in I and P*I, the vector returned should be
     # along [1.0, 1.0, 1.0] / sqrt(3.0) with a rotation of angle = 120/180 * pi
     rotate_from_matrix = np.eye(3).reshape(3, 3, 1)
-    rotate_to_matrix = np.roll(np.eye(3), -1, axis=1).reshape(3, 3, 1)
-    input_director_collection = np.dstack((rotate_from_matrix, rotate_to_matrix))
+    # Q_new = Q_old . R^T
+    rotate_to_matrix =  np.eye(3) @ np.roll(np.eye(3), -1, axis=1).T
+    input_director_collection = np.dstack((rotate_from_matrix, rotate_to_matrix.reshape(3, 3, -1)))
 
     correct_axis_collection = np.ones((3, 1)) / np.sqrt(3.0)
     test_axis_collection = _inv_rotate(input_director_collection)