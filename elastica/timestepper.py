9d401ef369aa0fe03f9f7e47e992f5ce59d3a284
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-10-24 23:33:06 -0500

    feat : LinearExponentialIntegrator, Explicit updates

--- elastica/timestepper.py
+++ elastica/timestepper.py
@@ -5,18 +5,18 @@ import functools
 from ._linalg import _batch_matmul, _batch_matvec, _batch_cross
 from .utils import Tolerance
 
-class TimeStepper():
+
+class TimeStepper:
     """ Interface classes for all time-steppers
     """
 
     def __init__(self):
         pass
 
-    def do_step(self):
-        pass
-
-    def _solve_linear_equation(self, time, dt, states, state_transition_matrix):
-        return _batch_matmul(states, state_transition_matrix)
+    def do_step(self, *args, **kwargs):
+        raise NotImplementedError(
+            "TimeStepper hierarchy is not supposed to access the do-step routine of the TimeStepper base class. "
+        )
 
 
 class ExplicitStepper(TimeStepper):
@@ -26,73 +26,96 @@ class ExplicitStepper(TimeStepper):
 
     def __init__(self):
         super(ExplicitStepper, self).__init__()
-        self.__stages = [v for (k, v) in self.__class__.__dict__.items() if k.endswith('stage')]
+        __stages = [
+            v for (k, v) in self.__class__.__dict__.items() if k.endswith("stage")
+        ]
+        __updates = [
+            v for (k, v) in self.__class__.__dict__.items() if k.endswith("update")
+        ]
 
         # Tuples are almost immutable
-        self._n_stages = (len(self.__stages),)
+        _n_stages = len(__stages)
+        _n_updates = len(__updates)
+
+        assert (
+            _n_stages == _n_updates
+        ), "Number of stages and updates should be equal to one another"
+
+        self.__stages_and_updates = tuple(zip(__stages, __updates))
 
     @property
     def n_stages(self):
-        return self._n_stages[0]
+        return len(self.__stages_and_updates)
 
     ### For stateless explicit steppers
-    def do_step(self, System, Memory, time : np.float64, dt : np.float64):
-        for stage in self.__stages:
-            time = stage(self, System, Memory, time, dt)
+    def do_step(self, System, Memory, time: np.float64, dt: np.float64):
+        for stage, update in self.__stages_and_updates:
+            stage(self, System, Memory, time, dt)
+            time = update(self, System, Memory, time, dt)
         return time
 
-class StatefulExplicitStepper():
 
+class StatefulExplicitStepper:
     def __init__(self):
         pass
 
     # For stateful steppes, bind memory to self
-    def do_step(self, System, time : np.float64, dt : np.float64):
+    def do_step(self, System, time: np.float64, dt: np.float64):
         return self.stepper.do_step(System, self, time, dt)
+
+    @property
+    def n_stages(self):
+        return self.stepper.n_stages
+
 
 class RungeKutta4(ExplicitStepper):
     """
     Stateless runge-kutta4. coordinates operations only, memory needs
     to be externally managed and allocated.
     """
+
     def __init__(self):
         super(RungeKutta4, self).__init__()
 
     # These methods should be static, but because we need to enable automatic
     # discovery in ExplicitStepper, these are bound to the RungeKutta4 class
     # For automatic discovery, the order of declaring stages here is very important
-    def _first_stage(self, System, Memory, time : np.float64, dt : np.float64):
+    def _first_stage(self, System, Memory, time: np.float64, dt: np.float64):
         Memory.initial_state = System.state.copy()
         # self.initial_state = 1
         Memory.k_1 = dt * System(time, dt)  # Don't update state yet
 
+    def _first_update(self, System, Memory, time: np.float64, dt: np.float64):
         # prepare for next stage
         System.state = Memory.initial_state + 0.5 * Memory.k_1
         return time + 0.5 * dt
 
-
-    def _second_stage(self, System, Memory, time : np.float64, dt : np.float64):
+    def _second_stage(self, System, Memory, time: np.float64, dt: np.float64):
         Memory.k_2 = dt * System(time, dt)  # Don't update state yet
 
+    def _second_update(self, System, Memory, time: np.float64, dt: np.float64):
         # prepare for next stage
         System.state = Memory.initial_state + 0.5 * Memory.k_2
         return time
 
-
-    def _third_stage(self, System, Memory, time : np.float64, dt : np.float64):
+    def _third_stage(self, System, Memory, time: np.float64, dt: np.float64):
         Memory.k_3 = dt * System(time, dt)  # Don't update state yet
 
+    def _third_update(self, System, Memory, time: np.float64, dt: np.float64):
+
         # prepare for next stage
-        time += 0.5 * dt
         System.state = Memory.initial_state + Memory.k_3
-        return time
+        return time + 0.5 * dt
 
+    def _fourth_stage(self, System, Memory, time: np.float64, dt: np.float64):
+        Memory.k_4 = dt * System(time, dt)  # Don't update state yet
 
-    def _fourth_stage(self, System, Memory, time : np.float64, dt : np.float64):
-        k_4 = dt * System(time, dt)  # Don't update state yet
-
+    def _fourth_update(self, System, Memory, time: np.float64, dt: np.float64):
         # prepare for next stage
-        System.state = Memory.initial_state + (Memory.k_1 + 2. * Memory.k_2 + 2. * Memory.k_3 + k_4) / 6.0
+        System.state = (
+            Memory.initial_state
+            + (Memory.k_1 + 2.0 * Memory.k_2 + 2.0 * Memory.k_3 + Memory.k_4) / 6.0
+        )
         return time
 
 
@@ -103,6 +126,7 @@ class StatefulRungeKutta4(StatefulExplicitStepper):
 
     Convenience wrapper around Stateless that provides memory
     """
+
     def __init__(self):
         super(StatefulRungeKutta4, self).__init__()
         self.stepper = RungeKutta4()
@@ -110,23 +134,58 @@ class StatefulRungeKutta4(StatefulExplicitStepper):
         self.k_1 = None
         self.k_2 = None
         self.k_3 = None
+        self.k_4 = None
+
 
 """
 Demonstration of constructing a staged-method 
 using EulerForward Timestepper
 """
+
+
 class EulerForward(ExplicitStepper):
     def __init__(self):
         super(EulerForward, self).__init__()
 
     def _first_stage(self, System, Memory, time, dt):
+        pass
+
+    def _first_update(self, System, Memory, time, dt):
         System.state += dt * System(time, dt)
         return time + dt
+
 
 class StatefulEulerForward(StatefulExplicitStepper):
     def __init__(self):
         super(StatefulEulerForward, self).__init__()
         self.stepper = EulerForward()
+
+
+class LinearExponentialIntegrator(ExplicitStepper):
+    def __init__(self):
+        super(LinearExponentialIntegrator, self).__init__()
+
+    def _do_stage(self, System, Memory, time, dt):
+        # TODO : Make more general, system should not be calculating what the state
+        # transition matrix directly is, but rather it should just give
+        Memory.linear_operator = System.get_linear_state_transition_operator(time, dt)
+
+    def _do_update(self, System, Memory, time, dt):
+        # System.linearly_evolving_state = _batch_matmul(
+        #     System.linearly_evolving_state,
+        #     Memory.linear_operator
+        # )
+        System.linearly_evolving_state = np.einsum(
+            "ijk,ljk->ilk", System.linearly_evolving_state, Memory.linear_operator
+        )
+        return time + dt
+
+
+class StatefulLinearExponentialIntegrator(StatefulExplicitStepper):
+    def __init__(self):
+        super(StatefulLinearExponentialIntegrator, self).__init__()
+        self.stepper = LinearExponentialIntegrator()
+        self.linear_operator = None
 
 
 # TODO Improve interface of this function to take args and kwargs for ease of use
--- tests/test_timestepper.py
+++ tests/test_timestepper.py
@@ -1,5 +1,3 @@
-__doc__ = """ Test all timestepper kernels and interfaces here """
-
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose
@@ -8,9 +6,13 @@ from elastica.timestepper import (
     StatefulRungeKutta4,
     StatefulEulerForward,
     StatefulExplicitStepper,
+    TimeStepper,
+    StatefulLinearExponentialIntegrator,
     integrate,
 )
 from elastica.utils import Tolerance
+from elastica._rotations import _get_rotation_matrix
+from elastica._linalg import _batch_matmul
 
 
 class BaseStatefulSystem:
@@ -23,6 +25,19 @@ class BaseStatefulSystem:
 
     @state.setter
     def state(self, new_state):
+        self._state = new_state
+
+
+class BaseLinearStatefulSystem:
+    def __init__(self):
+        pass
+
+    @property
+    def linearly_evolving_state(self):
+        return self._state
+
+    @linearly_evolving_state.setter
+    def linearly_evolving_state(self, new_state):
         self._state = new_state
 
 
@@ -104,21 +119,57 @@ class DampedSimpleHarmonicOscillatorSystem(UndampedSimpleHarmonicOscillatorSyste
         return np.array([analytical_position, analytical_velocity])
 
 
+class MultipleFrameRotationSystem(BaseLinearStatefulSystem):
+    def __init__(self, n_frames=128):
+        super(MultipleFrameRotationSystem, self).__init__()
+        self.initial_value = np.tile(
+            np.eye(3, 3).reshape(3, 3, 1), n_frames
+        )  # Start aligned initially
+        self.omega = np.random.randn(3, n_frames)  # Randomly rotate frames
+        # self.omega /= np.norm(self.omega, ord=2, axis=0)
+        self._state = self.initial_value.copy()
+
+    def analytical_solution(self, time):
+        # http://scipp.ucsc.edu/~haber/ph5B/sho09.pdf
+        # return _batch_matmul(self._state, _get_rotation_matrix(time, self.omega))
+        return np.einsum(
+            "ijk,ljk->ilk", self.initial_value, _get_rotation_matrix(time, self.omega)
+        )
+
+    def get_linear_state_transition_operator(self, time, dt):
+        return _get_rotation_matrix(dt, self.omega)
+
+
 # Test cases with several simple ODE's with
 # changing systems and states to test accuracy
 # of our stepper
 class TestExplicitSteppers:
     # Added automatic discovery of Stateful explicit integrators
-    ExplicitSteppers = StatefulExplicitStepper.__subclasses__()
+    # ExplicitSteppers = StatefulExplicitStepper.__subclasses__()
+    ExplicitSteppers = [StatefulRungeKutta4, StatefulEulerForward]
+
+    def test_no_base_access_error(self):
+        with pytest.raises(NotImplementedError) as excinfo:
+            TimeStepper().do_step()
+        assert "not supposed to access" in str(excinfo.value)
+
+    @pytest.mark.parametrize("stepper", ExplicitSteppers)
+    def test_correct_orders(self, stepper):
+        assert stepper().n_stages > 0, "Explicit stepper routine has no stages!"
 
     @pytest.mark.parametrize("stepper", ExplicitSteppers)
     def test_against_scalar_exponential(self, stepper):
         system = ScalarExponentialDecaySystem(-1, 1)
-        final_time = 0.1
+        final_time = 1
         n_steps = 1000
         integrate(stepper(), system, final_time=final_time, n_steps=n_steps)
 
-        assert_allclose(system.state, system.analytical_solution(final_time), atol=1e-4)
+        assert_allclose(
+            system.state,
+            system.analytical_solution(final_time),
+            rtol=Tolerance.rtol() * 1e3,
+            atol=Tolerance.atol(),
+        )
 
     @pytest.mark.parametrize("stepper", ExplicitSteppers[:-1])
     def test_against_undamped_harmonic_oscillator(self, stepper):
@@ -127,7 +178,12 @@ class TestExplicitSteppers:
         n_steps = 2000
         integrate(stepper(), system, final_time=final_time, n_steps=n_steps)
 
-        assert_allclose(system.state, system.analytical_solution(final_time), atol=1e-4)
+        assert_allclose(
+            system.state,
+            system.analytical_solution(final_time),
+            rtol=Tolerance.rtol(),
+            atol=Tolerance.atol(),
+        )
 
     @pytest.mark.parametrize("stepper", ExplicitSteppers[:-1])
     def test_against_damped_harmonic_oscillator(self, stepper):
@@ -136,4 +192,26 @@ class TestExplicitSteppers:
         n_steps = 2000
         integrate(stepper(), system, final_time=final_time, n_steps=n_steps)
 
-        assert_allclose(system.state, system.analytical_solution(final_time), atol=1e-4)
+        assert_allclose(
+            system.state,
+            system.analytical_solution(final_time),
+            rtol=Tolerance.rtol(),
+            atol=Tolerance.atol(),
+        )
+
+    def test_linear_exponential_integrator(self):
+        system = MultipleFrameRotationSystem(n_frames=128)
+        final_time = np.pi
+        n_steps = 1000
+        integrate(
+            StatefulLinearExponentialIntegrator(),
+            system,
+            final_time=final_time,
+            n_steps=n_steps,
+        )
+
+        assert_allclose(
+            system.linearly_evolving_state,
+            system.analytical_solution(final_time),
+            atol=1e-4,
+        )
