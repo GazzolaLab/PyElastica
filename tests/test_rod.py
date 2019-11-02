4e00f17fc8a6ca8a4dff11d8f9592d165c6a9b5e
Bhosale <bhosale2@illinois.edu>
2019-11-01 22:04:48 -0500

    test class for rod

---
+++ tests/test_rod.py
@@ -0,0 +1,9 @@
+__doc__ = """ Rod class for testing module """
+
+
+class TestRod:
+    def __init__(self):
+        self.position = 0
+        self.directors = 0
+        self.velocity = 0
+        self.omega = 0
