b90abe7688748453becdd89a1bf4b5d4ce621ab1
armantekinalp <armant2@illinois.edu>
2019-11-04 12:14:15 -0600

    Helical buckling case boundary conditions and tests added

--- elastica/boundary_conditions.py
+++ elastica/boundary_conditions.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from ._rod import *
 from ._linalg import _batch_matmul, _batch_matvec, _batch_cross
-
+from elastica._rotations import _get_rotation_matrix
 
 # the base class for rod boundary conditions
 # also the free rod class
@@ -33,3 +33,60 @@ class OneEndFixedRod(FreeRod):
     def neumann(self):
         self.rod.velocity[..., 0] = 0
         self.rod.omega[..., 0] = 0
+
+# start of the helical buckling bc
+class HelicalBucklingBC(FreeRod):
+    def __init__(self, rod, twisting_time, D, R, direction):
+        self.rod = rod
+        self.twisting_time = twisting_time
+        self.D = D
+        self.R = R
+        self.angel_vel_scalar = (2.0 * R * np.pi / self.twisting_time) / 2.0
+        self.shrink_vel_scalar = self.D / (self.twisting_time * 2.0)
+
+        self.direction = direction
+
+        self.final_startX = self.rod.position[...,0]  + self.D / 2.0 * self.direction
+        self.final_endX   = self.rod.position[...,-1] - self.D / 2.0 * self.direction
+
+        self.ang_vel = self.angel_vel_scalar * self.direction
+        self.shrink_vel = self.shrink_vel_scalar * self.direction
+
+        theta = R * np.pi
+
+        self.final_startQ = _get_rotation_matrix(theta, self.direction.reshape(3,1)).reshape(3,3)\
+                            @ self.rod.directors[..., 0] # rotation_matrix wants vectors 3,1
+        self.final_endQ   = _get_rotation_matrix(-theta, self.direction.reshape(3,1)).reshape(3,3)\
+                            @ self.rod.directors[..., -1] # rotation_matrix wants vectors 3,1
+
+    def dirichlet(self, time):
+        if time > self.twisting_time:
+            self.rod.position[...,0]  = self.final_startX
+            self.rod.position[...,-1] = self.final_endX
+
+            self.rod.directors[...,0]  = self.final_startQ
+            self.rod.directors[...,-1] = self.final_endQ
+
+
+    def neumann(self, time):
+        if time > self.twisting_time:
+            self.rod.velocity[...,0]  = 0.0
+            self.rod.omega[...,0]     = 0.0
+
+            self.rod.velocity[...,-1] = 0.0
+            self.rod.omega[...,-1]    = 0.0
+
+        else:
+            self.rod.velocity[...,0]  =  self.shrink_vel
+            self.rod.omega[...,0]     =  self.ang_vel
+
+            self.rod.velocity[...,-1]  = -self.shrink_vel
+            self.rod.velocity[...,-1]  = -self.ang_vel
+
+
+
+
+
+
+
+
--- tests/test_boundary_conditions.py
+++ tests/test_boundary_conditions.py
@@ -5,7 +5,7 @@ sys.path.append("..")
 # System imports
 import numpy as np
 from test_rod import TestRod
-from elastica.boundary_conditions import FreeRod, OneEndFixedRod
+from elastica.boundary_conditions import FreeRod, OneEndFixedRod, HelicalBucklingBC
 from numpy.testing import assert_allclose, assert_array_equal
 from elastica.utils import Tolerance
 from elastica._linalg import _batch_matmul, _batch_matvec, _batch_cross
@@ -66,6 +66,88 @@ def test_one_end_fixed_rod():
     assert_allclose(test_velocity, test_rod.velocity,
                     atol=Tolerance.atol())
     assert_allclose(test_omega, test_rod.omega,
+                    atol=Tolerance.atol())
+
+
+def test_helical_buckling_bc():
+
+    twisting_time = 500.0
+    D = 3.0
+    R = 27.0
+    direction = np.array([1.0, 0., 0,])
+    start_position = np.array([0., 0., 0.])
+    start_directors = np.identity(3,float)
+    end_position = np.array([100., 0., 0.])
+    end_directiors = np.identity(3,float)
+
+    test_rod = TestRod()
+
+    test_position = np.random.rand(3, 20)
+    test_position[...,0]  = start_position
+    test_position[...,-1] = end_position
+    test_rod.position = test_position
+    test_directors = np.tile(np.identity(3,float),20).reshape(3,3,20)
+    test_directors[...,0] = start_directors
+    test_directors[...,-1] = end_directiors
+    test_rod.directors = test_directors
+
+    test_velocity = np.random.rand(3, 20)
+    test_rod.velocity = test_velocity
+    test_omega = np.random.rand(3, 20)
+    test_rod.omega = test_omega
+
+    helicalbuckling_rod = (HelicalBucklingBC(test_rod, twisting_time,
+                            D, R, direction))
+
+    # Check Neumann BC
+    # time < twisting time
+    time = twisting_time - 1.0
+
+    helicalbuckling_rod.neumann(time=time)
+    test_velocity[..., 0] = np.array([1.5, 0., 0.])
+    test_velocity[..., -1] = -np.array([1.5, 0., 0.])
+    test_omega[..., 0] = np.array([0.169646, 0., 0.])
+    test_omega[..., -1] = -np.array([0.169646, 0., 0.])
+
+    assert_allclose(test_velocity, test_rod.velocity,
+                    atol=Tolerance.atol())
+    assert_allclose(test_omega, test_rod.omega,
+                    atol=Tolerance.atol())
+
+    # time > twisting time
+    time = twisting_time + 1
+    helicalbuckling_rod.neumann(time=time)
+    test_velocity[..., 0] = np.array((0, 0, 0))
+    test_velocity[..., -1] = np.array((0, 0, 0))
+    test_omega[..., 0] = np.array((0, 0, 0))
+    test_omega[...,-1] = np.array((0, 0, 0))
+    assert_allclose(test_velocity, test_rod.velocity,
+                    atol=Tolerance.atol())
+    assert_allclose(test_omega, test_rod.omega,
+                    atol=Tolerance.atol())
+
+
+    # Check Dirichlet BC
+    helicalbuckling_rod.dirichlet(time=time)
+
+    test_position[...,0]  = 1.5
+    test_position[...,-1] = 98.5
+
+    test_directors[...,0] = np.array([
+                                            [1.0, 0.0, 0.0],
+                                            [0.0, -1.0, -6.85926004e-15],
+                                            [0.0, 6.85926004e-15, -1.0]
+                                    ])
+
+    test_directors[..., -1] = np.array([
+                                            [1.0, 0.0, 0.0],
+                                            [0.0, -1.0, 6.85926004e-15],
+                                            [0.0, -6.85926004e-15, -1.0]
+                                    ])
+
+    assert_allclose(test_position, test_rod.position,
+                    atol=Tolerance.atol())
+    assert_allclose(test_directors, test_rod.directors,
                     atol=Tolerance.atol())
 
 
