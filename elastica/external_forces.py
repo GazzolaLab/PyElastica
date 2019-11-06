2cb3eb65138a902eef08dcd905073fc6bd7e001b
Bhosale <bhosale2@illinois.edu>
2019-11-05 18:10:33 -0600

    added standard style comments

--- elastica/external_forces.py
+++ elastica/external_forces.py
@@ -3,9 +3,11 @@ __doc__ = """ External forcing for rod """
 import numpy as np
 
 
-# the base class for rod external forcing
-# also the no forcing class
 class NoForces:
+    """
+    the base class for rod external forcing
+    also the no forcing class
+    """
     def __init__(self, rod):
         self.rod = rod
 
@@ -16,8 +18,10 @@ class NoForces:
         pass
 
 
-# apply gravity on the list of rod
 class GravityForces(NoForces):
+    """
+    apply gravity on the list of rod
+    """
     def __init__(self, rod, gravity):
         NoForces.__init__(self, rod)
         self.gravity = gravity
@@ -26,9 +30,11 @@ class GravityForces(NoForces):
         self.rod.external_forces += np.outer(self.gravity, self.rod.mass)
 
 
-# puts constant forces on endpoints
-# can be modified for temporal variation
 class EndpointForces(NoForces):
+    """
+    puts constant forces on endpoints
+    can be modified for temporal variation
+    """
     def __init__(self, rod, start_force, end_force):
         NoForces.__init__(self, rod)
         self.start_force = start_force
