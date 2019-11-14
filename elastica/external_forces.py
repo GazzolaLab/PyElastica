c6bae2192057e938f7f15f6e412f262e7e6abe01
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-11-14 16:49:22 -0600

    fix : external_forces to adapt to interface
    For compliance with changes made in #27

--- elastica/external_forces.py
+++ elastica/external_forces.py
@@ -4,45 +4,72 @@ import numpy as np
 
 
 class NoForces:
-    """
-    the base class for rod external forcing
-    also the no forcing class
+    """ Base class for external forcing for Rods
+
+    Can make this an abstract class, but its inconvenient
+    for the user to keep on defining apply_forces and
+    apply_torques object over and over.
     """
 
-    def __init__(self, rod):
-        self.rod = rod
-
-    def apply_forces(self):
+    def __init__(self):
         pass
 
-    def apply_torques(self):
+    def apply_forces(self, system, time: np.float = 0.0):
+        """ Apply forces to a system object.
+
+        In NoForces, this routine simply passes.
+
+        Parameters
+        ----------
+        system : system that is Rod-like
+        time : np.float, the time of simulation
+
+        Returns
+        -------
+        None
+
+        """
+
+        pass
+
+    def apply_torques(self, system, time: np.float = 0.0):
+        """ Apply torques to a Rod-like object.
+
+        In NoForces, this routine simply passes.
+
+        Parameters
+        ----------
+        system : system that is Rod-like
+        time : np.float, the time of simulation
+
+        Returns
+        -------
+        None
+        """
         pass
 
 
 class GravityForces(NoForces):
-    """
-    apply gravity on the list of rod
+    """ Applies a constant gravity on the entire rod
     """
 
-    def __init__(self, rod, gravity):
-        NoForces.__init__(self, rod)
-        self.gravity = gravity
+    def __init__(self, acc_gravity=np.array([0.0, 0.0, -9.80665])):
+        super(GravityForces, self).__init__()
+        self.acc_gravity = acc_gravity
 
-    def apply_forces(self):
-        self.rod.external_forces += np.outer(self.gravity, self.rod.mass)
+    def apply_forces(self, system, time=0.0):
+        system.external_forces += np.outer(self.acc_gravity, system.mass)
 
 
 class EndpointForces(NoForces):
-    """
-    puts constant forces on endpoints
-    can be modified for temporal variation
+    """ Applies constant forces on endpoints
     """
 
-    def __init__(self, rod, start_force, end_force):
-        NoForces.__init__(self, rod)
+    def __init__(self, start_force, end_force):
+        super(EndpointForces, self).__init__()
         self.start_force = start_force
         self.end_force = end_force
 
-    def apply_forces(self):
-        self.rod.external_forces[..., 0] += self.start_force
-        self.rod.external_forces[..., -1] += self.end_force
+    def apply_forces(self, system, time=0.0):
+        system.external_forces[..., 0] += self.start_force
+        system.external_forces[..., -1] += self.end_force
