b631669513901a88feea6d4b3afe1971f2b2f505
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-12-07 10:23:43 -0600

    fix : rename rod members to *_collection in test suite

--- tests/test_rod.py
+++ tests/test_rod.py
@@ -5,16 +5,20 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 from elastica.rod.data_structures import _bootstrap_from_data
+from elastica.utils import MaxDimension
 
 
 class TestRod:
     def __init__(self):
-        self.position = 0
-        self.directors = 0
-        self.velocity = 0
-        self.omega = 0
-        self.mass = 0
-        self.external_forces = 0
+        bs = 32
+        self.position_collection = np.random.randn(MaxDimension.value(), bs)
+        self.director_collection = np.random.randn(
+            MaxDimension.value(), MaxDimension.value(), bs
+        )
+        self.velocity_collection = np.random.randn(MaxDimension.value(), bs)
+        self.omega_collection = np.random.randn(MaxDimension.value(), bs)
+        self.mass = np.abs(np.random.randn(bs))
+        self.external_forces = np.zeros(bs)
 
 
 # Choosing 15 and 31 as nelems to reflect common expected
