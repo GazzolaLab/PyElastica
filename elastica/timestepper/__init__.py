c66ab780d68796d708859ed63458cd0e588af57f
Tejaswin Parthasarathy <tp5@illinois.edu>
2019-12-07 10:51:00 -0600

    feat : `integrate` enhancements
    1. Introduce `tqdm` as external dependency to visualize simulation
    progress
    2. Provide experimental support for callbacks using `CallBack` class
    3. Some code to collect statistics of simulations (will be deprecated
    soon)

--- elastica/timestepper/__init__.py
+++ elastica/timestepper/__init__.py
@@ -46,8 +46,40 @@ def extend_stepper_interface(Stepper, System):
     extend_instance(ConcreteStepper, ExtendClass)
 
 
+# TODO Add features to this experimental callback class
+class CallBack:
+    def __init__(self, step_skip: int, sys_idx, state_key):
+        self.every = step_skip
+        self.system_idx = sys_idx
+        self.state_key = state_key
+        self.callable = None
+
+    @classmethod
+    def make_callback(cls, state_key):
+        return cls(step_skip=1, sys_idx=0, state_key=state_key)
+
+    def will_be_called_at_every(self, step_count: int):
+        self.every = step_count
+
+    def needs(self, state_key: str):
+        self.state_key = state_key
+        return self
+
+    def of(self, sys_idx):
+        self.system_idx = sys_idx
+
+    def register(self, callable):
+        self.callable = callable
+
+
 # TODO Improve interface of this function to take args and kwargs for ease of use
-def integrate(StatefulStepper, System, final_time: float, n_steps: int = 1000):
+def integrate(
+    StatefulStepper,
+    System,
+    final_time: float,
+    n_steps: int = 1000,
+    callbacks: list = None,
+):
     assert final_time > 0.0, "Final time is negative!"
     assert n_steps > 0, "Number of integration steps is negative!"
 
@@ -61,6 +93,25 @@ def integrate(StatefulStepper, System, final_time: float, n_steps: int = 1000):
 
     dt = np.float64(float(final_time) / n_steps)
     time = np.float64(0.0)
-    tol = Tolerance.atol()
-    while np.abs(final_time - time) > 1e5 * tol:
+    # tol = Tolerance.atol()
+    # while np.abs(final_time - time) > 1e5 * tol:
+    from tqdm import tqdm
+
+    # TODO Remove in a future version, use callback instead
+    pos_memory_list = []
+    velocity_memory_list = []
+    dir_memory_list = []
+
+    for i in tqdm(range(n_steps)):
         time = StatefulStepper.do_step(System, time, dt)
+
+        # TODO Remove in a future version, use callback instead
+        # Uncomment if needed, causes tests to fail for
+        # simple systems, as expected
+        # if i % 1000 == 0:
+        #     pos_memory_list.append(System[0].position_collection.copy())
+        #     dir_memory_list.append(System[0].director_collection.copy())
+        #     velocity_memory_list.append(System[0].velocity_collection.copy())
+
+    print("Final time of simulation is : ", time)
+    return pos_memory_list, dir_memory_list, velocity_memory_list
