13b0ca8278d8e2206af0d6a43025b2ca41a38af9
armantekinalp <armant2@illinois.edu>
2019-12-12 23:42:18 -0600

    enhencement and organization:TimoshenkoBeamCase
    This commit organizes Timoshenko test case and pushed related files in
    to `TimoshenkoBeamCase` folder. In order to post process data to
    generate figures common plotting functions used by `timoshenko.py` and
    `convergence_timoshenko.py` are put in `timoshenko_postprocessing.py`
    file. Also, some unused lines and code repeats deleted.

--- TimoshenkoBeamCase/convergence_timoshenko.py
+++ TimoshenkoBeamCase/convergence_timoshenko.py
@@ -1,6 +1,9 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.colors import to_rgb
+
+# FIXME without appending sys.path make it more generic
+import sys
+
+sys.path.append("../")
 
 from elastica.wrappers import BaseSystemCollection, Connections, Constraints, Forcing
 from elastica.rod.cosserat_rod import CosseratRod
@@ -8,20 +11,34 @@ from elastica.boundary_conditions import OneEndFixedRod, FreeRod
 from elastica.external_forces import EndpointForces
 from elastica.timestepper.symplectic_steppers import PositionVerlet, PEFRL
 from elastica.timestepper import integrate
+from TimoshenkoBeamCase.timoshenko_postprocessing import (
+    plot_timoshenko,
+    analytical_shearable,
+)
+from convergence_functions import calculate_error_norm, plot_convergence
 
 
 class TimoshenkoBeamSimulator(BaseSystemCollection, Constraints, Forcing):
     pass
 
 
-def simulate_timoshenko_beam_with(elements=10, draw=False, add_unshearable_rod=False):
+# Options
+PLOT_FIGURE = True
+SAVE_FIGURE = False
+SAVE_RESULTS = False
+ADD_UNSHEARABLE_ROD = False
+
+
+def simulate_timoshenko_beam_with(
+    elements=10, PLOT_FIGURE=False, ADD_UNSHEARABLE_ROD=False
+):
     timoshenko_sim = TimoshenkoBeamSimulator()
     final_time = 5000.0
     # setting up test params
     n_elem = elements
     start = np.zeros((3,))
-    direction = np.array([0., 0., 1.])
-    normal = np.array([0., 1., 0.])
+    direction = np.array([0.0, 0.0, 1.0])
+    normal = np.array([0.0, 1.0, 0.0])
     base_length = 3.0
     base_radius = 0.25
     base_area = np.pi * base_radius ** 2
@@ -45,11 +62,13 @@ def simulate_timoshenko_beam_with(elements=10, draw=False, add_unshearable_rod=F
     )
 
     timoshenko_sim.append(shearable_rod)
-    timoshenko_sim.constrain(shearable_rod).using(OneEndFixedRod, positions=(0,), directors=(0,))
+    timoshenko_sim.constrain(shearable_rod).using(
+        OneEndFixedRod, positions=(0,), directors=(0,)
+    )
     end_force = np.array([-15.0, 0.0, 0.0])
-    timoshenko_sim.add_forcing_to(shearable_rod).using(EndpointForces, 0.0 * end_force, end_force, rampupTime=final_time/2)
-
-    ADD_UNSHEARABLE_ROD = add_unshearable_rod
+    timoshenko_sim.add_forcing_to(shearable_rod).using(
+        EndpointForces, 0.0 * end_force, end_force, rampupTime=final_time / 2
+    )
 
     if ADD_UNSHEARABLE_ROD:
         # Start into the plane
@@ -69,80 +88,45 @@ def simulate_timoshenko_beam_with(elements=10, draw=False, add_unshearable_rod=F
         )
 
         timoshenko_sim.append(unshearable_rod)
-        timoshenko_sim.constrain(unshearable_rod).using(OneEndFixedRod, positions=(0,), directors=(0,))
-        timoshenko_sim.add_forcing_to(unshearable_rod).using(EndpointForces, 0.0 * end_force, end_force)
+        timoshenko_sim.constrain(unshearable_rod).using(
+            OneEndFixedRod, positions=(0,), directors=(0,)
+        )
+        timoshenko_sim.add_forcing_to(unshearable_rod).using(
+            EndpointForces, 0.0 * end_force, end_force, rampupTime=final_time / 2
+        )
 
     timoshenko_sim.finalize()
     timestepper = PositionVerlet()
     # timestepper = PEFRL()
 
-    positions_over_time = []
-    directors_over_time = []
-    velocities_over_time = []
-
     dl = base_length / n_elem
     dt = 0.01 * dl
     total_steps = int(final_time / dt)
     print("Total steps", total_steps)
-    positions_over_time, directors_over_time, velocities_over_time = integrate(timestepper, timoshenko_sim, final_time,
-                                                                               total_steps)
+    positions_over_time, directors_over_time, velocities_over_time = integrate(
+        timestepper, timoshenko_sim, final_time, total_steps
+    )
 
-    # positions_over_time, directors_over_time, velocities_over_time = integrate(timestepper, timoshenko_sim, 500.0, int(8e5))
-    # positions_over_time, directors_over_time, velocities_over_time = integrate(timestepper, timoshenko_sim, 5.0, int(5e3))
+    if PLOT_FIGURE:
+        plot_timoshenko(shearable_rod, end_force, SAVE_FIGURE, ADD_UNSHEARABLE_ROD)
 
-    def analytical_shearable(arg_s, arg_end_force, arg_rod):
-        if type(arg_end_force) is np.ndarray:
-            acting_force = arg_end_force[np.nonzero(arg_end_force)]
-        else:
-            acting_force = arg_end_force
-        acting_force = np.abs(acting_force)
-
-        linear_prefactor = -acting_force / arg_rod.shear_matrix[0, 0, 0]
-        quadratic_prefactor = -acting_force * np.sum(arg_rod.rest_lengths) / 2.0 / arg_rod.bend_matrix[0, 0, 0]
-        cubic_prefactor = acting_force / 6.0 / arg_rod.bend_matrix[0, 0, 0]
-        return arg_s * (linear_prefactor + arg_s * (quadratic_prefactor + arg_s * cubic_prefactor))
-
-    def analytical_unshearable(arg_s, arg_end_force, arg_rod):
-        if type(arg_end_force) is np.ndarray:
-            acting_force = arg_end_force[np.nonzero(arg_end_force)]
-        else:
-            acting_force = arg_end_force
-        acting_force = np.abs(acting_force)
-
-        quadratic_prefactor = -acting_force * np.sum(arg_rod.rest_lengths) / 2.0 / arg_rod.bend_matrix[0, 0, 0]
-        cubic_prefactor = acting_force / 6.0 / arg_rod.bend_matrix[0, 0, 0]
-        return arg_s ** 2 * (quadratic_prefactor + arg_s * cubic_prefactor)
-
-    # calculate errors
-    centerline = np.linspace(0.0, base_length, n_elem + 1)  # count nodes
-    error = shearable_rod.position_collection[0, ...] - analytical_shearable(centerline, end_force, shearable_rod)
-
-    if draw:
-        centerline = np.linspace(0.0, base_length, 500)  # higher sampling rate
-        fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
-        fig.clear()
-        ax = fig.add_subplot(111)
-        n_pos = len(positions_over_time)
-        # for i_pos, position in enumerate(positions_over_time):
-        #     ax.plot(position[2, ...], position[0, ...], c='b', alpha=10**((i_pos/n_pos)-1))
-        ax.plot(centerline, analytical_shearable(centerline, end_force, shearable_rod), 'k--', lw=2)
-        ax.plot(shearable_rod.position_collection[2, ...], shearable_rod.position_collection[0, ...], c='b', lw=2)
-        if ADD_UNSHEARABLE_ROD:
-            ax.plot(centerline, analytical_unshearable(centerline, end_force, unshearable_rod), 'k--', lw=2)
-            ax.plot(unshearable_rod.position_collection[2, ...], unshearable_rod.position_collection[0, ...], c='r',
-                    lw=2)
-        # ax.set_aspect('equal')
-
-        # vel_fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
-        # vel_fig.clear()
-        # vel_ax = vel_fig.add_subplot(111)
-        # velocity_norms = [np.linalg.norm(v, 2) for v in velocities_over_time]
-        # vel_ax.plot(velocity_norms)
-        #
-        plt.show()
-
-    return {'rod': shearable_rod, 'position_history': positions_over_time, 'velocity_history': velocities_over_time,
-            'director_history': directors_over_time, 'error': error}
+    # calculate errors and norms
+    # Since we need to evaluate analytical solution only on nodes, n_nodes = n_elems+1
+    error, l1, l2, linf = calculate_error_norm(
+        analytical_shearable(shearable_rod, end_force, n_elem + 1)[1],
+        shearable_rod.position_collection[0, ...],
+        n_elem,
+    )
+    return {
+        "rod": shearable_rod,
+        "position_history": positions_over_time,
+        "velocity_history": velocities_over_time,
+        "director_history": directors_over_time,
+        "error": error,
+        "l1": l1,
+        "l2": l2,
+        "linf": linf,
+    }
 
 
 if __name__ == "__main__":
@@ -151,7 +135,7 @@ if __name__ == "__main__":
     # 5, 6, ... 9
     convergence_elements = list(range(5, 10))
     # 10, 20, ... , 100
-    convergence_elements.extend([10 * x for x in range(1,11)])
+    convergence_elements.extend([10 * x for x in range(1, 11)])
     convergence_elements.extend([200])
 
     # Convergence study
@@ -159,22 +143,14 @@ if __name__ == "__main__":
     with mp.Pool(mp.cpu_count()) as pool:
         results = pool.map(simulate_timoshenko_beam_with, convergence_elements)
 
-    errors = {'l1': [], 'l2': [], 'linf': []}
-    # results is a dict containing entries needed for post_processing
-    for n_elem, result in zip(convergence_elements, results):
-        print("final velocity norm at {} is {}".format(n_elem, np.linalg.norm(result['rod'].velocity_collection)))
-        errors['l1'].append(np.linalg.norm(result['error'], 1)/n_elem)
-        errors['l2'].append(np.linalg.norm(result['error'], 2)/n_elem)
-        errors['linf'].append(np.linalg.norm(result['error'], np.inf))
+    if PLOT_FIGURE:
+        filename = "Timoshenko_convergence_test.png"
+        plot_convergence(results, SAVE_FIGURE, filename)
 
-    fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
-    ax = fig.add_subplot(111)
-    ax.grid(b=True, which='minor', color='k', linestyle='--')
-    ax.grid(b=True, which='major', color='k', linestyle='-')
-    ax.loglog(convergence_elements, errors['l1'], marker='o', ms=10, c=to_rgb("xkcd:bluish"), lw=2, label="l1")
-    ax.loglog(convergence_elements, errors['l2'], marker='o', ms=10, c=to_rgb("xkcd:reddish"), lw=2, label="l2")
-    ax.loglog(convergence_elements, errors['linf'], marker='o', ms=10, c='k', lw=2, label="linf")
-    fig.legend(prop={'size':20})
-    fig.show()
-    fig.savefig("Timoshenko_convergence_test")
+    if SAVE_RESULTS:
+        import pickle
 
+        filename = "Timoshenko_convergence_test_data.dat"
+        file = open(filename, "wb")
+        pickle.dump([results], file)
+        file.close()
--- TimoshenkoBeamCase/timoshenko.py
+++ TimoshenkoBeamCase/timoshenko.py
@@ -1,5 +1,9 @@
 import numpy as np
-from matplotlib import pyplot as plt
+
+# FIXME without appending sys.path make it more generic
+import sys
+
+sys.path.append("../")
 
 from elastica.wrappers import BaseSystemCollection, Connections, Constraints, Forcing
 from elastica.rod.cosserat_rod import CosseratRod
@@ -7,6 +11,7 @@ from elastica.boundary_conditions import OneEndFixedRod, FreeRod
 from elastica.external_forces import EndpointForces
 from elastica.timestepper.symplectic_steppers import PositionVerlet, PEFRL
 from elastica.timestepper import integrate
+from TimoshenkoBeamCase.timoshenko_postprocessing import plot_timoshenko
 
 
 class TimoshenkoBeamSimulator(BaseSystemCollection, Constraints, Forcing):
@@ -14,17 +19,24 @@ class TimoshenkoBeamSimulator(BaseSystemCollection, Constraints, Forcing):
 
 
 timoshenko_sim = TimoshenkoBeamSimulator()
+final_time = 5000
+
+# Options
+PLOT_FIGURE = True
+SAVE_FIGURE = False
+SAVE_RESULTS = False
+ADD_UNSHEARABLE_ROD = False
 
 # setting up test params
 n_elem = 100
 start = np.zeros((3,))
-direction = np.array([0., 0., 1.])
-normal = np.array([0., 1., 0.])
+direction = np.array([0.0, 0.0, 1.0])
+normal = np.array([0.0, 1.0, 0.0])
 base_length = 3.0
 base_radius = 0.25
 base_area = np.pi * base_radius ** 2
 density = 5000
-nu = 0.1#2.9
+nu = 0.1
 E = 1e6
 # For shear modulus of 1e4, nu is 99!
 poisson_ratio = 99
@@ -42,15 +54,16 @@ shearable_rod = CosseratRod.straight_rod(
     poisson_ratio,
 )
 
-final_time = 5000
-
 timoshenko_sim.append(shearable_rod)
-timoshenko_sim.constrain(shearable_rod).using(OneEndFixedRod, positions=(0,), directors=(0,))
-# end_force = np.array([-15.0, 0.0, 0.0])
-end_force = np.array([-15.0, 0.0, 0.0])
-timoshenko_sim.add_forcing_to(shearable_rod).using(EndpointForces, 0.0 * end_force, end_force, rampupTime=5000/2)
+timoshenko_sim.constrain(shearable_rod).using(
+    OneEndFixedRod, positions=(0,), directors=(0,)
+)
 
-ADD_UNSHEARABLE_ROD = False
+end_force = np.array([-15.0, 0.0, 0.0])
+timoshenko_sim.add_forcing_to(shearable_rod).using(
+    EndpointForces, 0.0 * end_force, end_force, rampupTime=final_time / 2.0
+)
+
 
 if ADD_UNSHEARABLE_ROD:
     # Start into the plane
@@ -70,78 +83,32 @@ if ADD_UNSHEARABLE_ROD:
     )
 
     timoshenko_sim.append(unshearable_rod)
-    timoshenko_sim.constrain(unshearable_rod).using(OneEndFixedRod, positions=(0,), directors=(0,))
-    timoshenko_sim.add_forcing_to(unshearable_rod).using(EndpointForces, 0.0 * end_force, end_force, rampupTime=final_time/2.0)
+    timoshenko_sim.constrain(unshearable_rod).using(
+        OneEndFixedRod, positions=(0,), directors=(0,)
+    )
+    timoshenko_sim.add_forcing_to(unshearable_rod).using(
+        EndpointForces, 0.0 * end_force, end_force, rampupTime=final_time / 2.0
+    )
 
 timoshenko_sim.finalize()
 timestepper = PositionVerlet()
 # timestepper = PEFRL()
 
-positions_over_time = []
-directors_over_time = []
-velocities_over_time = []
-# final_time = 2000.0
-dl = base_length/n_elem
-dt = 0.01*dl
-# dt = 3.0/40 * 0.01
-total_steps = int(final_time/dt)
+dl = base_length / n_elem
+dt = 0.01 * dl
+total_steps = int(final_time / dt)
 print("Total steps", total_steps)
-positions_over_time, directors_over_time, velocities_over_time = integrate(timestepper, timoshenko_sim, final_time, total_steps)
-# positions_over_time, directors_over_time, velocities_over_time = integrate(timestepper, timoshenko_sim, 500.0, int(8e5))
-# positions_over_time, directors_over_time, velocities_over_time = integrate(timestepper, timoshenko_sim, 5.0, int(5e3))
+positions_over_time, directors_over_time, velocities_over_time = integrate(
+    timestepper, timoshenko_sim, final_time, total_steps
+)
 
-def analytical_shearable(arg_s, arg_end_force, arg_rod):
-    if type(arg_end_force) is np.ndarray:
-        acting_force = arg_end_force[np.nonzero(arg_end_force)]
-    else:
-        acting_force = arg_end_force
-    acting_force = np.abs(acting_force)
+if PLOT_FIGURE:
+    plot_timoshenko(shearable_rod, end_force, SAVE_FIGURE, ADD_UNSHEARABLE_ROD)
 
-    linear_prefactor = -acting_force / arg_rod.shear_matrix[0, 0, 0]
-    quadratic_prefactor = -acting_force * np.sum(arg_rod.rest_lengths) / 2.0 / arg_rod.bend_matrix[0, 0, 0]
-    cubic_prefactor = acting_force / 6.0 / arg_rod.bend_matrix[0, 0, 0]
-    return arg_s * (linear_prefactor + arg_s * (quadratic_prefactor + arg_s * cubic_prefactor))
+if SAVE_RESULTS:
+    import pickle
 
-
-def analytical_unshearable(arg_s, arg_end_force, arg_rod):
-    if type(arg_end_force) is np.ndarray:
-        acting_force = arg_end_force[np.nonzero(arg_end_force)]
-    else:
-        acting_force = arg_end_force
-    acting_force = np.abs(acting_force)
-
-    quadratic_prefactor = -acting_force * np.sum(arg_rod.rest_lengths) / 2.0 / arg_rod.bend_matrix[0, 0, 0]
-    cubic_prefactor = acting_force / 6.0 / arg_rod.bend_matrix[0, 0, 0]
-    return arg_s ** 2 * (quadratic_prefactor + arg_s * cubic_prefactor)
-
-
-fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
-fig.clear()
-ax = fig.add_subplot(111)
-n_pos = len(positions_over_time)
-# for i_pos, position in enumerate(positions_over_time):
-#     ax.plot(position[2, ...], position[0, ...], c='b', alpha=10**((i_pos/n_pos)-1))
-centerline = np.linspace(0.0, base_length, 500)
-ax.plot(centerline, analytical_shearable(centerline, end_force, shearable_rod), 'k--', lw=2)
-ax.plot(shearable_rod.position_collection[2, ...], shearable_rod.position_collection[0, ...], c='b', lw=2)
-if ADD_UNSHEARABLE_ROD:
-    ax.plot(centerline, analytical_unshearable(centerline, end_force, unshearable_rod), 'k--', lw=2)
-    ax.plot(unshearable_rod.position_collection[2, ...], unshearable_rod.position_collection[0, ...], c='r', lw=2)
-# ax.set_aspect('equal')
-
-# vel_fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
-# vel_fig.clear()
-# vel_ax = vel_fig.add_subplot(111)
-# velocity_norms = [np.linalg.norm(v, 2) for v in velocities_over_time]
-# vel_ax.plot(velocity_norms)
-#
-plt.show()
-
-com = []
-fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
-bx = fig.add_subplot(111)
-for position in positions_over_time[-10:]:
-    bx.plot(position[2, ...], position[0, ...])
-    com.append(np.sum(position * shearable_rod.mass, axis=1) / np.sum(shearable_rod.mass))
-bx.plot(centerline, analytical_shearable(centerline, end_force, shearable_rod), 'k--', lw=2)
-plt.show()
+    filename = "Timoshenko_beam_data.dat"
+    file = open(filename, "wb")
+    pickle.dump(shearable_rod, file)
+    file.close()
