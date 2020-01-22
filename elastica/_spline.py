b2eaa15af7a7db48a33fadd8281e812f9814f2a6
armantekinalp <armant2@illinois.edu>
2020-01-22 17:24:15 -0600

    enhencement: adding continuum snake muscle torques
    This commit adds muscle torque implementation for continuum snake, with
    unit test cases. Muscle torque implementation is in `external_forces.py`
    file and corresponding unit tests are located in
    `test_external_forces.py` file. Here since we need a beta-spline for
    muscle torque profile, we are using the scipy beta spline functions.
    These functions are located in `_spline.py` file.

---
+++ elastica/_spline.py
@@ -0,0 +1,77 @@
+__doc__ = """ Spline for muscle torques acting on rod """
+import numpy as np
+
+
+def _bspline(t_coeff, l_centerline=1.0, keep_pts=False):
+    """ Generates a bspline object that plots the spline interpolant for
+    any vector x. Optionally takes in a centerline length, set to 1.0 by
+    default and keep_pts for keeping record of control points
+
+    Parameters
+    ----------
+    t_coeff : np.array
+        The spline coefficients, denoted by :math:`beta_i`. Note that the first
+        and the last values are set to zero by default.
+    l_centreline : float
+        The length of the centerline in meters.
+    keep_pts : boolean, optional
+        If True, we keep record of control point and coefficients at those points
+        by returned as a (3, ) tuple. If False, returns only the splibe object.
+        Defaults to False.
+
+    Returns
+    -------
+    spline : scipy.interpolate.Bspline class
+        A spline class that can be called as spline(x), where x are the points at
+        which the spline needs to be evaluated.
+    """
+    # Divide into n_control_pts numbrer of points (n_ctr_pts-1) regions
+    control_pts = l_centerline * np.linspace(0.0, 1.0, t_coeff.shape[0])
+
+    # Degree of b-spline required. Set to cubic
+    degree = 3
+
+    # Update coefficients at the head and tail
+    # Setting it to 0.0 here
+    beta_head = 0.0
+    beta_tail = 1.0
+
+    if keep_pts:
+        # Keep record of control point and coefficient at those points
+        return __bspline_impl__(control_pts, t_coeff, beta_head, beta_tail, degree)
+    else:
+        return __bspline_impl__(control_pts, t_coeff, beta_head, beta_tail, degree)[0]
+
+
+def __bspline_impl__(x_pts, t_c, b_head, b_tail, t_k):
+    """
+        """
+    from scipy.interpolate import BSpline
+
+    # Update the coefficients
+    #
+    t_c = np.hstack((b_head, t_c, b_tail))
+
+    # Update the knots
+    # You need 2 additional knots for the head and tail control points
+    # You need degree + 1 additional knots to sink into the head and tail for
+    # controlling C^k smoothness. We set it to 0.0
+    n_upd = x_pts.shape[0] + 2 + (t_k + 1)
+
+    # The first and last points are always fixed
+    x_first = b_head
+    x_last = b_tail
+    x_pts = np.hstack((x_first, x_pts, x_last))
+
+    # Generate the knots
+    knots_updated = np.zeros(n_upd,)
+    # Sink first degree-1 knots into head
+    knots_updated[: t_k - 1] = x_first
+    # Middle knot locations correspond to x_locations
+    knots_updated[t_k - 1 : n_upd - (t_k - 1)] = x_pts
+    # Sink first degree-1 knots into tail
+    knots_updated[n_upd - (t_k - 1) :] = x_last
+
+    # print(knots_updated)
+
+    return BSpline(knots_updated, t_c, t_k, extrapolate=False), x_pts, t_c
--- tests/test_external_forces.py
+++ tests/test_external_forces.py
@@ -12,6 +12,7 @@ from elastica.external_forces import (
     EndpointForces,
     UniformTorques,
     UniformForces,
+    MuscleTorques,
 )
 from elastica.utils import Tolerance
 
@@ -21,6 +22,7 @@ def mock_rod_init(self):
     self.external_forces = 0.0
     self.external_torques = 0.0
     self.director_collection = 0.0
+    self.rest_lengths = 0.0
 
 
 MockRod = type("MockRod", (object,), {"__init__": mock_rod_init})
@@ -151,3 +153,63 @@ def test_uniform_forces(n_elem, forces, time=0.0):
     uniform_forces.apply_forces(mock_rod, time)
 
     assert_allclose(mock_rod.external_forces.sum(), force, atol=Tolerance.atol())
+
+
+# Now test muscle torques
+@pytest.mark.parametrize("n_elem", [3, 4, 16])
+def test_muscle_torques(n_elem):
+    # tests muscle torques
+    dim = 3
+
+    mock_rod = MockRod()
+    mock_rod.external_torques = np.zeros((dim, n_elem))
+    mock_rod.n_elems = n_elem
+    mock_rod.director_collection = np.repeat(
+        np.identity(3)[:, :, np.newaxis], n_elem, axis=2
+    )
+
+    base_length = 1.0
+    mock_rod.rest_lengths = np.ones(n_elem) * base_length / n_elem
+
+    # We wont need beta coefficients, thus we are setting them to zero.
+    # The reason is that, we wont use beta spline for this test case.
+    # Beta spline function does not have any pytest, It is compared with
+    # Elastica cpp beta spline and coefficients are optimized using CMA-ES.
+    b_coeff = np.zeros((n_elem))
+
+    # Torque = sin(2pi*t/T + 2pi*s/lambda + phi)
+    # Torque = sin(pi/2 + s) = cos(s) using the below variables
+    # we will make torque as function of position only nothing else!
+    period = 8.0
+    wave_length = 2.0 * np.pi
+    phase_shift = np.pi / 4
+    rampupTime = 0.5  # this has to be smaller than 1. Since I will set t equal to 1
+    time = 1.0
+
+    position = np.linspace(0, base_length, n_elem + 1)
+    torque = np.array([np.cos(position), np.cos(position), np.cos(position)])[..., 1:-1]
+    correct_torque = np.zeros((dim, n_elem))
+    correct_torque[..., :-1] -= torque
+    correct_torque[..., 1:] += torque
+
+    # Set an a non-physical direction to check math
+    direction = np.array([1.0, 1.0, 1.0])
+
+    # Apply torques
+    muscletorques = MuscleTorques(
+        base_length,
+        b_coeff,
+        period,
+        wave_length,
+        phase_shift,
+        rampupTime,
+        direction,
+        WithSpline=False,
+    )
+    muscletorques.apply_torques(mock_rod, time)
+
+    # Total torque has to be zero on the body
+    assert_allclose(mock_rod.external_torques[..., :].sum(), 0.0, atol=Tolerance.atol())
+
+    # Torques on elements
+    assert_allclose(mock_rod.external_torques, correct_torque, atol=Tolerance.atol())
