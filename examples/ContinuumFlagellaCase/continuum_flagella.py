c4f919e2d8ec7307cadc824fded4bbdd7e2579ea
armantekinalp <armant2@illinois.edu>
2020-01-29 14:15:08 -0600

    enhencement:CMA optimization option added flagella
    This commit adds CMA optimization option to continuum flagella case.
    Also we changed the post processing scripts for flagella. Now velocity
    profile is plotted using projection procedure.

--- examples/ContinuumFlagellaCase/continuum_flagella.py
+++ examples/ContinuumFlagellaCase/continuum_flagella.py
@@ -21,9 +21,10 @@ from elastica.interaction import SlenderBodyTheory
 from elastica.callback_functions import CallBackBaseClass
 from elastica.timestepper.symplectic_steppers import PositionVerlet, PEFRL
 from elastica.timestepper import integrate
-from ContinuumFlagellaCase.continuum_flagella_postprocessing import (
+from examples.ContinuumFlagellaCase.continuum_flagella_postprocessing import (
     plot_velocity,
     plot_video,
+    compute_projected_velocity,
 )
 
 
@@ -31,134 +32,194 @@ class FlagellaSimulator(BaseSystemCollection, Constraints, Forcing, CallBacks):
     pass
 
 
-flagella_sim = FlagellaSimulator()
+def run_flagella(
+    b_coeff, PLOT_FIGURE=False, SAVE_FIGURE=False, SAVE_VIDEO=False, SAVE_RESULTS=False,
+):
+
+    flagella_sim = FlagellaSimulator()
+
+    # setting up test params
+    n_elem = 50
+    start = np.zeros((3,))
+    direction = np.array([0.0, 0.0, 1.0])
+    normal = np.array([0.0, 1.0, 0.0])
+    base_length = 1.0
+    base_radius = 0.025
+    base_area = np.pi * base_radius ** 2
+    density = 1000
+    nu = 5.0
+    E = 1e7
+    poisson_ratio = 0.5
+
+    shearable_rod = CosseratRod.straight_rod(
+        n_elem,
+        start,
+        direction,
+        normal,
+        base_length,
+        base_radius,
+        density,
+        nu,
+        E,
+        poisson_ratio,
+    )
+
+    flagella_sim.append(shearable_rod)
+    flagella_sim.constrain(shearable_rod).using(FreeRod)
+
+    period = 1.0
+    # TODO: wave_length is also part of optimization, when we integrate with CMA-ES
+    # remove wave_length from here.
+    wave_length = 0.3866575573648976 * base_length  # wave number is 16.25
+    flagella_sim.add_forcing_to(shearable_rod).using(
+        MuscleTorques,
+        base_length=base_length,
+        b_coeff=b_coeff,
+        period=period,
+        wave_number=2.0 * np.pi / (wave_length),
+        phase_shift=0.0,
+        ramp_up_time=period,
+        direction=normal,
+        with_spline=True,
+    )
+
+    # Add slender body forces
+    fluid_density = 1.0
+    reynolds_number = 1e-4
+    dynamic_viscosity = (
+        fluid_density * base_length * base_length / (period * reynolds_number)
+    )
+    flagella_sim.add_forcing_to(shearable_rod).using(
+        SlenderBodyTheory, dynamic_viscosity=dynamic_viscosity
+    )
+
+    # Add call backs
+    class ContinuumFlagellaCallBack(CallBackBaseClass):
+        """
+        Call back function for continuum snake
+        """
+
+        def __init__(self, step_skip: int, callback_params):
+            CallBackBaseClass.__init__(self)
+            self.every = step_skip
+            self.callback_params = callback_params
+
+        def make_callback(self, system, time, current_step: int):
+
+            if current_step % self.every == 0:
+
+                self.callback_params["time"].append(time)
+                self.callback_params["step"].append(current_step)
+                self.callback_params["position"].append(
+                    system.position_collection.copy()
+                )
+                self.callback_params["velocity"].append(
+                    system.velocity_collection.copy()
+                )
+                self.callback_params["avg_velocity"].append(
+                    system.compute_velocity_center_of_mass()
+                )
+                self.callback_params["center_of_mass"].append(
+                    system.compute_position_center_of_mass()
+                )
+
+                return
+
+    pp_list = {
+        "time": [],
+        "step": [],
+        "position": [],
+        "velocity": [],
+        "avg_velocity": [],
+        "center_of_mass": [],
+    }
+    flagella_sim.callback_of(shearable_rod).using(
+        ContinuumFlagellaCallBack, step_skip=200, callback_params=pp_list
+    )
+
+    flagella_sim.finalize()
+    timestepper = PositionVerlet()
+    # timestepper = PEFRL()
+
+    final_time = (10.0 + 0.01) * period
+    dt = 2.5e-5 * period
+    total_steps = int(final_time / dt)
+    print("Total steps", total_steps)
+    # FIXME: remove integrate outputs, we have call back functions now, we dont need them.
+    positions_over_time, directors_over_time, velocities_over_time = integrate(
+        timestepper, flagella_sim, final_time, total_steps
+    )
+
+    if PLOT_FIGURE:
+        filename_plot = "continuum_flagella_velocity.png"
+        plot_velocity(pp_list, period, filename_plot, SAVE_FIGURE)
+
+        if SAVE_VIDEO:
+            filename_video = "continuum_flagella.mp4"
+            plot_video(pp_list, video_name=filename_video, margin=0.2, fps=500)
+
+    if SAVE_RESULTS:
+        import pickle
+
+        filename = "continuum_flagella.dat"
+        file = open(filename, "wb")
+        pickle.dump(pp_list, file)
+        file.close()
+
+    # Compute the average forward velocity. These will be used for optimization.
+    [_, _, avg_forward, avg_lateral] = compute_projected_velocity(pp_list, period)
+
+    return avg_forward, avg_lateral, pp_list
 
 
-# Options
-PLOT_FIGURE = True
-SAVE_FIGURE = True
-SAVE_VIDEO = True
-SAVE_RESULTS = True
+if __name__ == "__main__":
 
+    # Options
+    PLOT_FIGURE = True
+    SAVE_FIGURE = False
+    SAVE_VIDEO = False
+    SAVE_RESULTS = False
+    CMA_OPTION = False
 
-# setting up test params
-n_elem = 50
-start = np.zeros((3,))
-direction = np.array([0.0, 0.0, 1.0])
-normal = np.array([0.0, 1.0, 0.0])
-base_length = 1.0
-base_radius = 0.025
-base_area = np.pi * base_radius ** 2
-density = 1000
-nu = 5.0
-E = 1e7
-poisson_ratio = 0.5
+    if CMA_OPTION:
+        import cma
 
-shearable_rod = CosseratRod.straight_rod(
-    n_elem,
-    start,
-    direction,
-    normal,
-    base_length,
-    base_radius,
-    density,
-    nu,
-    E,
-    poisson_ratio,
-)
+        SAVE_OPTIMIZED_COEFFICIENTS = False
 
-flagella_sim.append(shearable_rod)
-flagella_sim.constrain(shearable_rod).using(FreeRod)
-
-# Add muscle forces on the rod
-if os.path.exists("optimized_coefficients.txt"):
-    t_coeff_optimized = np.genfromtxt("optimized_coefficients.txt", delimiter=",")
-else:
-    t_coeff_optimized = np.array([17.4, 48.5, 5.4, 14.7])
-period = 1.0
-# TODO: wave_length is also part of optimization, when we integrate with CMA-ES
-# remove wave_length from here.
-wave_length = 0.3866575573648976 * base_length  # wave number is 16.25
-flagella_sim.add_forcing_to(shearable_rod).using(
-    MuscleTorques,
-    base_length=base_length,
-    b_coeff=t_coeff_optimized,
-    period=period,
-    wave_number=2.0 * np.pi / (wave_length),
-    phase_shift=0.0,
-    ramp_up_time=period,
-    direction=normal,
-    with_spline=True,
-)
-
-# Add slender body forces
-fluid_density = 1.0
-reynolds_number = 1e-4
-dynamic_viscosity = (
-    fluid_density * base_length * base_length / (period * reynolds_number)
-)
-flagella_sim.add_forcing_to(shearable_rod).using(
-    SlenderBodyTheory, dynamic_viscosity=dynamic_viscosity
-)
-
-
-# Add call backs
-class ContinuumFlagellaCallBack(CallBackBaseClass):
-    """
-    Call back function for continuum snake
-    """
-
-    def __init__(self, step_skip: int, callback_params):
-        CallBackBaseClass.__init__(self)
-        self.every = step_skip
-        self.callback_params = callback_params
-
-    def make_callback(self, system, time, current_step: int):
-
-        if current_step % self.every == 0:
-
-            self.callback_params["time"].append(time)
-            self.callback_params["step"].append(current_step)
-            self.callback_params["position"].append(system.position_collection.copy())
-            self.callback_params["velocity"].append(system.velocity_collection.copy())
-            self.callback_params["avg_velocity"].append(
-                system.compute_velocity_center_of_mass()
+        def optimize_snake(spline_coefficient):
+            [avg_forward, _, _] = run_flagella(
+                spline_coefficient,
+                PLOT_FIGURE=False,
+                SAVE_FIGURE=False,
+                SAVE_VIDEO=False,
+                SAVE_RESULTS=False,
             )
+            return -avg_forward
 
-            return
+        # Optimize snake for forward velocity. In cma.fmin first input is function
+        # to be optimized, second input is initial guess for coefficients you are optimizing
+        # for and third input is standard deviation you initially set.
+        optimized_spline_coefficients = cma.fmin(optimize_snake, 5 * [0], 0.5)
 
+        # Save the optimized coefficients to a file
+        filename_data = "optimized_coefficients.txt"
+        if SAVE_OPTIMIZED_COEFFICIENTS:
+            assert filename_data != "", "provide a file name for coefficients"
+            np.savetxt(filename_data, optimized_spline_coefficients, delimiter=",")
 
-pp_list = {"time": [], "step": [], "position": [], "velocity": [], "avg_velocity": []}
-flagella_sim.callback_of(shearable_rod).using(
-    ContinuumFlagellaCallBack, step_skip=200, callback_params=pp_list
-)
+    else:
+        # Add muscle forces on the rod
+        if os.path.exists("optimized_coefficients.txt"):
+            t_coeff_optimized = np.genfromtxt(
+                "optimized_coefficients.txt", delimiter=","
+            )
+        else:
+            t_coeff_optimized = np.array([17.4, 48.5, 5.4, 14.7])
 
-flagella_sim.finalize()
-timestepper = PositionVerlet()
-# timestepper = PEFRL()
+        # run the simulation
+        [avg_forward, avg_lateral, pp_list] = run_flagella(
+            t_coeff_optimized, PLOT_FIGURE, SAVE_FIGURE, SAVE_VIDEO, SAVE_RESULTS
+        )
 
-final_time = (10.0 + 0.01) * period
-dt = 2.5e-5 * period
-total_steps = int(final_time / dt)
-print("Total steps", total_steps)
-# FIXME: remove integrate outputs, we have call back functions now, we dont need them.
-positions_over_time, directors_over_time, velocities_over_time = integrate(
-    timestepper, flagella_sim, final_time, total_steps
-)
-
-if PLOT_FIGURE:
-    filename_plot = "continuum_flagella_velocity.png"
-    plot_velocity(pp_list, period, filename_plot, SAVE_FIGURE)
-
-    if SAVE_VIDEO:
-        filename_video = "continuum_flagella.mp4"
-        plot_video(pp_list, video_name=filename_video, margin=0.2, fps=500)
-
-
-if SAVE_RESULTS:
-    import pickle
-
-    filename = "continuum_flagella.dat"
-    file = open(filename, "wb")
-    pickle.dump(pp_list, file)
-    file.close()
+        print("average forward velocity:", avg_forward)
+        print("average forward lateral:", avg_lateral)
--- examples/ContinuumFlagellaCase/continuum_flagella_postprocessing.py
+++ examples/ContinuumFlagellaCase/continuum_flagella_postprocessing.py
@@ -10,14 +10,23 @@ def plot_velocity(
     time_per_period = np.array(list_input["time"]) / period
     avg_velocity = np.array(list_input["avg_velocity"])
 
+    [
+        velocity_in_direction_of_rod,
+        velocity_in_rod_roll_dir,
+        _,
+        _,
+    ] = compute_projected_velocity(list_input, period)
+
     fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
     ax = fig.add_subplot(111)
     ax.grid(b=True, which="minor", color="k", linestyle="--")
     ax.grid(b=True, which="major", color="k", linestyle="-")
-    ax.plot(time_per_period[:], avg_velocity[:, 2], "r-", label="forward")
+    ax.plot(
+        time_per_period[:], velocity_in_direction_of_rod[:, 2], "r-", label="forward"
+    )
     ax.plot(
         time_per_period[:],
-        avg_velocity[:, 0],
+        velocity_in_rod_roll_dir[:, 0],
         c=to_rgb("xkcd:bluish"),
         label="lateral",
     )
@@ -51,3 +60,79 @@ def plot_video(
             plt.xlim([0 - margin, 2.5 + margin])
             plt.ylim([-1.25 - margin, 1.25 + margin])
             writer.grab_frame()
+
+
+def compute_projected_velocity(list_input, period):
+
+    time_per_period = np.array(list_input["time"]) / period
+    avg_velocity = np.array(list_input["avg_velocity"])
+    center_of_mass = np.array(list_input["center_of_mass"])
+
+    # Compute rod velocity in rod direction. We need to compute that because,
+    # after snake starts to move it chooses an arbitrary direction, which does not
+    # have to be initial tangent direction of the rod. Thus we need to project the
+    # snake velocity with respect to its new tangent and roll direction, after that
+    # we will get the correct forward and lateral speed. After this projection
+    # lateral velocity of the snake has to be oscillating between + and - values with
+    # zero mean.
+
+    # Number of steps in one period.
+    period_step = int(period / (time_per_period[-1] - time_per_period[-2])) + 1
+    number_of_period = int(time_per_period.shape[0] / period_step)
+    # Center of mass position averaged in one period
+    center_of_mass_averaged_over_one_period = np.zeros((number_of_period - 2, 3))
+    for i in range(1, number_of_period - 1):
+        # position of center of mass in rolling direction averaged over one period
+        center_of_mass_averaged_over_one_period[i - 1, 0] = np.mean(
+            center_of_mass[(i + 1) * period_step : (i + 2) * period_step, 0]
+            - center_of_mass[(i + 0) * period_step : (i + 1) * period_step, 0]
+        )
+        # position of center of mass in normal direction averaged over one period
+        center_of_mass_averaged_over_one_period[i - 1, 1] = np.mean(
+            center_of_mass[(i + 1) * period_step : (i + 2) * period_step, 1]
+            - center_of_mass[(i + 0) * period_step : (i + 1) * period_step, 1]
+        )
+        # position of center of mass in normal direction averaged over one period
+        center_of_mass_averaged_over_one_period[i - 1, 2] = np.mean(
+            center_of_mass[(i + 1) * period_step : (i + 2) * period_step, 2]
+            - center_of_mass[(i + 0) * period_step : (i + 1) * period_step, 2]
+        )
+
+    # Average the rod directions over multiple periods and get the direction of the rod.
+    direction_of_rod = np.array(
+        [
+            np.mean(center_of_mass_averaged_over_one_period[:, 0]),
+            np.mean(center_of_mass_averaged_over_one_period[:, 1]),
+            np.mean(center_of_mass_averaged_over_one_period[:, 2]),
+        ]
+    )
+    direction_of_rod /= np.linalg.norm(direction_of_rod, ord=2)
+
+    # Compute the projected rod velocity in the direction of the rod
+    velocity_mag_in_direction_of_rod = np.einsum(
+        "ji,i->j", avg_velocity, direction_of_rod
+    )
+    velocity_in_direction_of_rod = np.einsum(
+        "j,i->ji", velocity_mag_in_direction_of_rod, direction_of_rod
+    )
+
+    # Get the lateral or roll velocity of the rod after subtracting its projected
+    # velocity in the direction of rod
+    velocity_in_rod_roll_dir = avg_velocity - velocity_in_direction_of_rod
+
+    # Compute the average velocity over the simulation, this can be used for optimizing snake
+    # for fastest forward velocity. We start after first period, because of the ramping up happens
+    # in first period.
+    average_forward_velocity_over_simulation = np.mean(
+        velocity_in_direction_of_rod[period_step * 2 :, 2]
+    )
+    average_lateral_velocity_over_simulation = np.mean(
+        velocity_in_rod_roll_dir[period_step * 2 :, 0]
+    )
+
+    return (
+        velocity_in_direction_of_rod,
+        velocity_in_rod_roll_dir,
+        average_forward_velocity_over_simulation,
+        average_lateral_velocity_over_simulation,
+    )
