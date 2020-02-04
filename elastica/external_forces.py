ed86b51d17293be7fa2015dfaffb8bbfca482cd4
armantekinalp <armant2@illinois.edu>
2020-02-04 14:47:02 -0600

    rearragenment:`external_forces.py`
    This commit removes `EndpointForcesSinusoidal` class from the
    `external_forces.py` to joint case client files, because this class is
    specific to joints. Also here we  changes some functions `ramp_up_time`
    initialization. Now if user does not give `ramp_up_time` and if it is
    required by program by default `ramp_up_time` will be 0.

--- elastica/external_forces.py
+++ elastica/external_forces.py
@@ -72,10 +72,7 @@ class EndpointForces(NoForces):
         self.start_force = start_force
         self.end_force = end_force
         assert ramp_up_time >= 0.0
-        if ramp_up_time == 0:
-            self.ramp_up_time = 1e-14
-        else:
-            self.ramp_up_time = ramp_up_time
+        self.ramp_up_time = ramp_up_time
 
     def apply_forces(self, system, time=0.0):
         factor = min(1.0, time / self.ramp_up_time)
@@ -132,8 +129,8 @@ class MuscleTorques(NoForces):
         period,
         wave_number,
         phase_shift,
-        ramp_up_time,
         direction,
+        ramp_up_time=0.0,
         with_spline=False,
     ):
         super(MuscleTorques, self).__init__()
@@ -144,10 +141,7 @@ class MuscleTorques(NoForces):
         self.phase_shift = phase_shift
 
         assert ramp_up_time >= 0.0
-        if ramp_up_time == 0:
-            self.ramp_up_time = 1e-14
-        else:
-            self.ramp_up_time = ramp_up_time
+        self.ramp_up_time = ramp_up_time
 
         if with_spline:
             assert b_coeff.size != 0, "Beta spline coefficient array (t_coeff) is empty"
@@ -203,76 +197,3 @@ class MuscleTorques(NoForces):
         system.external_torques[..., :-1] -= _batch_matvec(
             system.director_collection[..., :-1], torque[..., 1:]
         )
-
-
-class EndpointForcesSinusoidal(NoForces):
-    """ Applies sinusoidal forces on endpoints
-    """
-
-    def __init__(
-        self,
-        start_force_mag,
-        end_force_mag,
-        ramp_up_time=0.0,
-        tangent_direction=np.array([0, 0, 1]),
-        normal_direction=np.array([0, 1, 0]),
-    ):
-        super(EndpointForcesSinusoidal, self).__init__()
-        # Start force
-        self.start_force_mag = start_force_mag
-        self.end_force_mag = end_force_mag
-
-        # Applied force directions
-        self.normal_direction = normal_direction
-        # self.roll_direction = np.cross(tangent_direction, normal_direction)
-        self.roll_direction = np.cross(normal_direction, tangent_direction)
-
-        assert ramp_up_time >= 0.0
-        if ramp_up_time == 0:
-            self.ramp_up_time = 1e-14
-        else:
-            self.ramp_up_time = ramp_up_time
-
-    def apply_forces(self, system, time=0.0):
-
-        if time < self.ramp_up_time:
-            # When time smaller than ramp up time apply the force in normal direction
-            # First pull the rod upward or downward direction some time.
-            start_force = -2.0 * self.start_force_mag * self.normal_direction
-            end_force = -2.0 * self.end_force_mag * self.normal_direction
-
-            system.external_forces[..., 0] += start_force
-            system.external_forces[..., -1] += end_force
-
-        else:
-            # When time is greater than ramp up time, forces are applied in normal
-            # and roll direction or forces are in a plane perpendicular to the
-            # direction.
-
-            # First force applied to start of the rod
-            roll_forces_start = (
-                self.start_force_mag
-                * np.cos(0.5 * np.pi * (time - self.ramp_up_time))
-                * self.roll_direction
-            )
-            normal_forces_start = (
-                self.start_force_mag
-                * np.sin(0.5 * np.pi * (time - self.ramp_up_time))
-                * self.normal_direction
-            )
-            start_force = roll_forces_start + normal_forces_start
-            # Now force applied to end of the rod
-            roll_forces_end = (
-                self.end_force_mag
-                * np.cos(0.5 * np.pi * (time - self.ramp_up_time))
-                * self.roll_direction
-            )
-            normal_forces_end = (
-                self.end_force_mag
-                * np.sin(0.5 * np.pi * (time - self.ramp_up_time))
-                * self.normal_direction
-            )
-            end_force = roll_forces_end + normal_forces_end
-            # Update external forces
-            system.external_forces[..., 0] += start_force
-            system.external_forces[..., -1] += end_force
