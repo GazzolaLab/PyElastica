f75daf3ebfd93118654c24b35bbe69039ea7d69a
armantekinalp <armant2@illinois.edu>
2020-02-28 11:17:00 -0600

    enhencement:muscle torque cal with spline
    This commit adds, a new function to compute muscle torques from splines
    using the njit decorated function. By this way we improved the speed of
    the code.

--- elastica/external_forces.py
+++ elastica/external_forces.py
@@ -5,120 +5,116 @@ from elastica._linalg import _batch_matvec
 from elastica._spline import _bspline
 
 
-class NoForces:
-    """ Base class for external forcing for Rods
-
-    Can make this an abstract class, but its inconvenient
-    for the user to keep on defining apply_forces and
-    apply_torques object over and over.
-    """
-
-    def __init__(self):
-        pass
-
-    def apply_forces(self, system, time: np.float = 0.0):
-        """ Apply forces to a system object.
-
-        In NoForces, this routine simply passes.
-
-        Parameters
-        ----------
-        system : system that is Rod-like
-        time : np.float, the time of simulation
-
-        Returns
-        -------
-        None
-
-        """
-
-        pass
-
-    def apply_torques(self, system, time: np.float = 0.0):
-        """ Apply torques to a Rod-like object.
-
-        In NoForces, this routine simply passes.
-
-        Parameters
-        ----------
-        system : system that is Rod-like
-        time : np.float, the time of simulation
-
-        Returns
-        -------
-        None
-        """
-        pass
-
-
-class GravityForces(NoForces):
-    """ Applies a constant gravity on the entire rod
-    """
-
-    def __init__(self, acc_gravity=np.array([0.0, -9.80665, 0.0])):
-        super(GravityForces, self).__init__()
-        self.acc_gravity = acc_gravity
-
-    def apply_forces(self, system, time=0.0):
-        system.external_forces += np.outer(self.acc_gravity, system.mass)
-
-
-class EndpointForces(NoForces):
-    """ Applies constant forces on endpoints
-    """
-
-    def __init__(self, start_force, end_force, ramp_up_time=0.0):
-        super(EndpointForces, self).__init__()
-        self.start_force = start_force
-        self.end_force = end_force
-        assert ramp_up_time >= 0.0
-        self.ramp_up_time = ramp_up_time
-
-    def apply_forces(self, system, time=0.0):
-        factor = min(1.0, time / self.ramp_up_time)
-
-        system.external_forces[..., 0] += self.start_force * factor
-        system.external_forces[..., -1] += self.end_force * factor
-
-
-class UniformTorques(NoForces):
-    """
-    Applies uniform torque to entire rod
-    """
-
-    def __init__(self, torque, direction=np.array([0.0, 0.0, 0.0])):
-        super(UniformTorques, self).__init__()
-        self.torque = (torque * direction).reshape(3, 1)
-
-    def apply_torques(self, system, time: np.float = 0.0):
-        torque_on_one_element = self.torque / system.n_elems
-        system.external_torques += _batch_matvec(
-            system.director_collection, torque_on_one_element
-        )
-
-
-class UniformForces(NoForces):
-    """
-    Applies uniform forces to entire rod
-    """
-
-    def __init__(self, force, direction=np.array([0.0, 0.0, 0.0])):
-        super(UniformForces, self).__init__()
-        self.force = (force * direction).reshape(3, 1)
-
-    def apply_forces(self, system, time: np.float = 0.0):
-        force_on_one_element = self.force / system.n_elems
-
-        system.external_forces += force_on_one_element
-
-        # Because mass of first and last node is half
-        system.external_forces[..., 0] -= 0.5 * force_on_one_element[:, 0]
-        system.external_forces[..., -1] -= 0.5 * force_on_one_element[:, 0]
-
-
 try:
     import numba
     from numba import njit
+    from elastica._linalg import _batch_product_i_k_to_ik
+
+    class NoForces:
+        """ Base class for external forcing for Rods
+
+        Can make this an abstract class, but its inconvenient
+        for the user to keep on defining apply_forces and
+        apply_torques object over and over.
+        """
+
+        def __init__(self):
+            pass
+
+        def apply_forces(self, system, time: np.float = 0.0):
+            """ Apply forces to a system object.
+
+            In NoForces, this routine simply passes.
+
+            Parameters
+            ----------
+            system : system that is Rod-like
+            time : np.float, the time of simulation
+
+            Returns
+            -------
+            None
+
+            """
+
+            pass
+
+        def apply_torques(self, system, time: np.float = 0.0):
+            """ Apply torques to a Rod-like object.
+
+            In NoForces, this routine simply passes.
+
+            Parameters
+            ----------
+            system : system that is Rod-like
+            time : np.float, the time of simulation
+
+            Returns
+            -------
+            None
+            """
+            pass
+
+    class GravityForces(NoForces):
+        """ Applies a constant gravity on the entire rod
+        """
+
+        def __init__(self, acc_gravity=np.array([0.0, -9.80665, 0.0])):
+            super(GravityForces, self).__init__()
+            self.acc_gravity = acc_gravity
+
+        def apply_forces(self, system, time=0.0):
+            system.external_forces += np.outer(self.acc_gravity, system.mass)
+
+    class EndpointForces(NoForces):
+        """ Applies constant forces on endpoints
+        """
+
+        def __init__(self, start_force, end_force, ramp_up_time=0.0):
+            super(EndpointForces, self).__init__()
+            self.start_force = start_force
+            self.end_force = end_force
+            assert ramp_up_time >= 0.0
+            self.ramp_up_time = ramp_up_time
+
+        def apply_forces(self, system, time=0.0):
+            factor = min(1.0, time / self.ramp_up_time)
+
+            system.external_forces[..., 0] += self.start_force * factor
+            system.external_forces[..., -1] += self.end_force * factor
+
+    class UniformTorques(NoForces):
+        """
+        Applies uniform torque to entire rod
+        """
+
+        def __init__(self, torque, direction=np.array([0.0, 0.0, 0.0])):
+            super(UniformTorques, self).__init__()
+            self.torque = (torque * direction).reshape(3, 1)
+
+        def apply_torques(self, system, time: np.float = 0.0):
+            torque_on_one_element = self.torque / system.n_elems
+            system.external_torques += _batch_matvec(
+                system.director_collection, torque_on_one_element
+            )
+
+    class UniformForces(NoForces):
+        """
+        Applies uniform forces to entire rod
+        """
+
+        def __init__(self, force, direction=np.array([0.0, 0.0, 0.0])):
+            super(UniformForces, self).__init__()
+            self.force = (force * direction).reshape(3, 1)
+
+        def apply_forces(self, system, time: np.float = 0.0):
+            force_on_one_element = self.force / system.n_elems
+
+            system.external_forces += force_on_one_element
+
+            # Because mass of first and last node is half
+            system.external_forces[..., 0] -= 0.5 * force_on_one_element[:, 0]
+            system.external_forces[..., -1] -= 0.5 * force_on_one_element[:, 0]
 
     class MuscleTorques(NoForces):
         """
@@ -148,6 +144,12 @@ try:
             assert ramp_up_time >= 0.0
             self.ramp_up_time = ramp_up_time
 
+            # s is the position of nodes on the rod, we go from node=1 to node=nelem-1, because there is no
+            # torques applied by first and last node on elements. Reason is that we cannot apply torque in an
+            # infinitesimal segment at the beginning and end of rod, because there is no additional element
+            # (at element=-1 or element=n_elem+1) to provide internal torques to cancel out an external
+            # torque. This coupled with the requirement that the sum of all muscle torques has
+            # to be zero results in this condition.
             self.s = np.cumsum(rest_lengths)
 
             if with_spline:
@@ -176,42 +178,178 @@ try:
                 self.my_spline = constant_function(self.s)
 
         def apply_torques(self, system, time: np.float = 0.0):
+            self.compute_muscle_torques(
+                time,
+                self.my_spline,
+                self.s,
+                self.angular_frequency,
+                self.wave_number,
+                self.phase_shift,
+                self.ramp_up_time,
+                self.direction.reshape(3),
+                system.director_collection,
+                system.external_torques,
+            )
 
+        @staticmethod
+        @njit()
+        def compute_muscle_torques(
+            time,
+            my_spline,
+            s,
+            angular_frequency,
+            wave_number,
+            phase_shift,
+            ramp_up_time,
+            direction,
+            director_collection,
+            external_torques,
+        ):
             # Ramp up the muscle torque
-            factor = min(1.0, time / self.ramp_up_time)
+            factor = min(1.0, time / ramp_up_time)
             # From the node 1 to node nelem-1
-            # s is the position of nodes on the rod, we go from node=1 to node=nelem-1, because there is no
-            # torques applied by first and last node on elements. Reason is that we cannot apply torque in an
-            # infinitesimal segment at the beginning and end of rod, because there is no additional element
-            # (at element=-1 or element=n_elem+1) to provide internal torques to cancel out an external
-            # torque. This coupled with the requirement that the sum of all muscle torques has
-            # to be zero results in this condition.
-            # s = np.cumsum(system.rest_lengths)
             # Magnitude of the torque. Am = beta(s) * sin(2pi*t/T + 2pi*s/lambda + phi)
             # There is an inconsistency with paper and Elastica cpp implementation. In paper sign in
             # front of wave number is positive, in Elastica cpp it is negative.
             torque_mag = (
                 factor
-                * self.my_spline
-                * np.sin(
-                    self.angular_frequency * time
-                    - self.wave_number * self.s
-                    + self.phase_shift
-                )
+                * my_spline
+                * np.sin(angular_frequency * time - wave_number * s + phase_shift)
             )
             # Head and tail of the snake is opposite compared to elastica cpp. We need to iterate torque_mag
             # from last to first element.
-            torque = np.einsum("j,ij->ij", torque_mag[::-1], self.direction)
-            # # TODO: Find a way without doing tow batch_matvec product
-            system.external_torques[..., 1:] += _batch_matvec(
-                system.director_collection, torque
-            )[..., 1:]
-            system.external_torques[..., :-1] -= _batch_matvec(
-                system.director_collection[..., :-1], torque[..., 1:]
+            torque = _batch_product_i_k_to_ik(direction, torque_mag[::-1])
+            inplace_addition(
+                external_torques[..., 1:],
+                _batch_matvec(director_collection, torque)[..., 1:],
             )
+            inplace_substraction(
+                external_torques[..., :-1],
+                _batch_matvec(director_collection[..., :-1], torque[..., 1:]),
+            )
+
+    @njit()
+    def inplace_addition(external_force_or_torque, force_or_torque):
+        blocksize = force_or_torque.shape[1]
+        for i in range(3):
+            for k in range(blocksize):
+                external_force_or_torque[i, k] += force_or_torque[i, k]
+
+    @njit()
+    def inplace_substraction(external_force_or_torque, force_or_torque):
+        blocksize = force_or_torque.shape[1]
+        for i in range(3):
+            for k in range(blocksize):
+                external_force_or_torque[i, k] -= force_or_torque[i, k]
 
 
 except ImportError:
+
+    class NoForces:
+        """ Base class for external forcing for Rods
+
+        Can make this an abstract class, but its inconvenient
+        for the user to keep on defining apply_forces and
+        apply_torques object over and over.
+        """
+
+        def __init__(self):
+            pass
+
+        def apply_forces(self, system, time: np.float = 0.0):
+            """ Apply forces to a system object.
+
+            In NoForces, this routine simply passes.
+
+            Parameters
+            ----------
+            system : system that is Rod-like
+            time : np.float, the time of simulation
+
+            Returns
+            -------
+            None
+
+            """
+
+            pass
+
+        def apply_torques(self, system, time: np.float = 0.0):
+            """ Apply torques to a Rod-like object.
+
+            In NoForces, this routine simply passes.
+
+            Parameters
+            ----------
+            system : system that is Rod-like
+            time : np.float, the time of simulation
+
+            Returns
+            -------
+            None
+            """
+            pass
+
+    class GravityForces(NoForces):
+        """ Applies a constant gravity on the entire rod
+        """
+
+        def __init__(self, acc_gravity=np.array([0.0, -9.80665, 0.0])):
+            super(GravityForces, self).__init__()
+            self.acc_gravity = acc_gravity
+
+        def apply_forces(self, system, time=0.0):
+            system.external_forces += np.outer(self.acc_gravity, system.mass)
+
+    class EndpointForces(NoForces):
+        """ Applies constant forces on endpoints
+        """
+
+        def __init__(self, start_force, end_force, ramp_up_time=0.0):
+            super(EndpointForces, self).__init__()
+            self.start_force = start_force
+            self.end_force = end_force
+            assert ramp_up_time >= 0.0
+            self.ramp_up_time = ramp_up_time
+
+        def apply_forces(self, system, time=0.0):
+            factor = min(1.0, time / self.ramp_up_time)
+
+            system.external_forces[..., 0] += self.start_force * factor
+            system.external_forces[..., -1] += self.end_force * factor
+
+    class UniformTorques(NoForces):
+        """
+        Applies uniform torque to entire rod
+        """
+
+        def __init__(self, torque, direction=np.array([0.0, 0.0, 0.0])):
+            super(UniformTorques, self).__init__()
+            self.torque = (torque * direction).reshape(3, 1)
+
+        def apply_torques(self, system, time: np.float = 0.0):
+            torque_on_one_element = self.torque / system.n_elems
+            system.external_torques += _batch_matvec(
+                system.director_collection, torque_on_one_element
+            )
+
+    class UniformForces(NoForces):
+        """
+        Applies uniform forces to entire rod
+        """
+
+        def __init__(self, force, direction=np.array([0.0, 0.0, 0.0])):
+            super(UniformForces, self).__init__()
+            self.force = (force * direction).reshape(3, 1)
+
+        def apply_forces(self, system, time: np.float = 0.0):
+            force_on_one_element = self.force / system.n_elems
+
+            system.external_forces += force_on_one_element
+
+            # Because mass of first and last node is half
+            system.external_forces[..., 0] -= 0.5 * force_on_one_element[:, 0]
+            system.external_forces[..., -1] -= 0.5 * force_on_one_element[:, 0]
 
     class MuscleTorques(NoForces):
         """
@@ -241,6 +379,12 @@ except ImportError:
             assert ramp_up_time >= 0.0
             self.ramp_up_time = ramp_up_time
 
+            # s is the position of nodes on the rod, we go from node=1 to node=nelem-1, because there is no
+            # torques applied by first and last node on elements. Reason is that we cannot apply torque in an
+            # infinitesimal segment at the beginning and end of rod, because there is no additional element
+            # (at element=-1 or element=n_elem+1) to provide internal torques to cancel out an external
+            # torque. This coupled with the requirement that the sum of all muscle torques has
+            # to be zero results in this condition.
             self.s = np.cumsum(rest_lengths)
 
             if with_spline:
@@ -273,13 +417,6 @@ except ImportError:
             # Ramp up the muscle torque
             factor = min(1.0, time / self.ramp_up_time)
             # From the node 1 to node nelem-1
-            # s is the position of nodes on the rod, we go from node=1 to node=nelem-1, because there is no
-            # torques applied by first and last node on elements. Reason is that we cannot apply torque in an
-            # infinitesimal segment at the beginning and end of rod, because there is no additional element
-            # (at element=-1 or element=n_elem+1) to provide internal torques to cancel out an external
-            # torque. This coupled with the requirement that the sum of all muscle torques has
-            # to be zero results in this condition.
-            # s = np.cumsum(system.rest_lengths)
             # Magnitude of the torque. Am = beta(s) * sin(2pi*t/T + 2pi*s/lambda + phi)
             # There is an inconsistency with paper and Elastica cpp implementation. In paper sign in
             # front of wave number is positive, in Elastica cpp it is negative.
