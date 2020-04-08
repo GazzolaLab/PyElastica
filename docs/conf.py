72e98fab212eaf87890625fbb3839bd2a54521d3
Noel Naughton <noel.mnaughton@gmail.com>
2020-04-08 17:41:51 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = 'index'
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
