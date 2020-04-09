72852e9f3f33c2ab2c9e5f67a8759a73249641d3
Noel Naughton <nnaught2@illinois.edu>
2020-04-09 08:20:56 -0500

    one more attempt at favicon

--- docs/conf.py
+++ docs/conf.py
@@ -88,6 +88,6 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_favicon = 'elastica_logo.ico'
+html_favicon = 'elastica_logo.svg'
 
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
