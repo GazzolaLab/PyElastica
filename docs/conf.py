bab4ca39f79f16e2f9bec5b467ae8eaf5ccbb8c9
Noel Naughton <noel.mnaughton@gmail.com>
2020-04-08 20:22:01 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ source_suffix = {
 # source_parsers = { ".md": "recommonmark.parser.CommonMarkParser",}
 # source_suffix = [".rst", ".md"]
 # -- Options for HTML output -------------------------------------------------
-html_favicon = 'elastica_logo.svg'
+
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -84,4 +84,5 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
+html_favicon = 'elastica_logo.svg'
