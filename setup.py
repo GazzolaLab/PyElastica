0fdd2a9362d001e9f1bac28f9788cbf172498664
armantekinalp <53585636+armantekinalp@users.noreply.github.com>
2020-11-02 12:06:30 -0600

    Update setup.py
    python 3.4 removed from classifiers.

--- setup.py
+++ setup.py
@@ -100,7 +100,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
