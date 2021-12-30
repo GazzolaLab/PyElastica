50ec58c5a8edb85ecb55639e7dce2f967f79b517
Seung Hyun Kim <skim449@illinois.edu>
2021-12-30 15:13:58 -0600

    update documentation version

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,11 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'Elastica'
-copyright = '2020, Gazzola Lab'
+copyright = '2022, Gazzola Lab'
 author = 'Gazzola Lab'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
