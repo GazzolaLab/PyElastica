81aa8c51eaa44de0a2cec05ef93d74f2a0589b0d
Seung Hyun Kim <skim0119@gmail.com>
2022-01-25 02:23:38 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2022, Gazzola Lab'
 author = 'Gazzola Lab'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
