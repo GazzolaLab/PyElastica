b720a1b79e026fbd783c636cff6575e854eedad3
Seung Hyun Kim <skim449@illinois.edu>
2022-02-10 16:26:03 -0600

    remove _elastica_numba warning messages

---
+++ elastica/_elastica_numba.py
@@ -0,0 +1,6 @@
+class _elastica_numpy:
+    """The purpose is to throw deprecation error to people previously
+    using _elastica_numba module. Please remove this in the future.
+    """
+
+    raise ImportError("The module _elastica_numba is moved to main implementation. Please import the modules without _elastica_numba.")
