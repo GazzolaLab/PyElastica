962113795944bfdd5895720291253d11f59e559b
Bhosale <bhosale2@illinois.edu>
2022-04-19 12:40:01 -0500

    feat: generic magnetic forcing class: apply torques

--- MagneticElastica/magnetic_forces.py
+++ MagneticElastica/magnetic_forces.py
@@ -6,14 +6,22 @@ from elastica.external_forces import NoForces
 
 
 class MagneticTorquesForUniformMagneticField(NoForces):
-    def __init__(self, ramp_interval,start_time, end_time, magnetization_density, magnetic_field_vector):
+    def __init__(
+        self,
+        ramp_interval,
+        start_time,
+        end_time,
+        magnetization_density,
+        magnetic_field_vector,
+    ):
         self.ramp_interval = ramp_interval
         self.start_time = start_time
         self.end_time = end_time
 
         self.magnetization_density = magnetization_density
-        self.magnetic_field_vector = np.ones((magnetization_density.shape[-1])) * magnetic_field_vector.reshape(3, 1)
-
+        self.magnetic_field_vector = np.ones(
+            (magnetization_density.shape[-1])
+        ) * magnetic_field_vector.reshape(3, 1)
 
     def apply_forces(self, system, time: np.float64 = 0.0):
         # No forces are applied since magnetic field is constant
@@ -28,13 +36,15 @@ class MagneticTorquesForUniformMagneticField(NoForces):
             factor = min(1.0, (time - self.start_time) / self.ramp_interval)
 
         if time > self.end_time:
-            factor = max(
-                0.0, -1 / self.ramp_interval * (time - self.end_time) + 1.0
-            )
+            factor = max(0.0, -1 / self.ramp_interval * (time - self.end_time) + 1.0)
 
         if factor > 0.0:
-            magnetization_vector = self.magnetization_density*system.volume*system.tangents
-            magnetic_torques = factor * _batch_cross(magnetization_vector, self.magnetic_field_vector)
+            magnetization_vector = (
+                self.magnetization_density * system.volume * system.tangents
+            )
+            magnetic_torques = factor * _batch_cross(
+                magnetization_vector, self.magnetic_field_vector
+            )
             np.round_(magnetic_torques, 12, magnetic_torques)
 
             system.external_torques += _batch_matvec(
@@ -43,7 +53,17 @@ class MagneticTorquesForUniformMagneticField(NoForces):
 
 
 class MagneticTorquesForOscillatingMagneticField(NoForces):
-    def __init__(self, ramp_interval, start_time, end_time, start_idx, end_idx, magnetization_density, magnetic_field_vector, frequency):
+    def __init__(
+        self,
+        ramp_interval,
+        start_time,
+        end_time,
+        start_idx,
+        end_idx,
+        magnetization_density,
+        magnetic_field_vector,
+        frequency,
+    ):
 
         self.ramp_interval = ramp_interval
         self.start_time = start_time
@@ -51,13 +71,14 @@ class MagneticTorquesForOscillatingMagneticField(NoForces):
 
         self.start_idx = start_idx
         self.end_idx = end_idx
-        n_elem = end_idx-start_idx
+        n_elem = end_idx - start_idx
 
-        self.magnetization_density = magnetization_density*np.ones((n_elem))
-        self.magnetic_field_vector = np.ones((n_elem)) * magnetic_field_vector.reshape(3, 1)
+        self.magnetization_density = magnetization_density * np.ones((n_elem))
+        self.magnetic_field_vector = np.ones((n_elem)) * magnetic_field_vector.reshape(
+            3, 1
+        )
 
-
-        self.wave_number = 2*np.pi*frequency
+        self.wave_number = 2 * np.pi * frequency
 
     def apply_torques(self, system, time: np.float64 = 0.0):
         factor = 0.0
@@ -67,21 +88,58 @@ class MagneticTorquesForOscillatingMagneticField(NoForces):
             factor = min(1.0, (time - self.start_time) / self.ramp_interval)
 
         if time > self.end_time:
-            factor = max(
-                0.0, -1 / self.ramp_interval * (time - self.end_time) + 1.0
-            )
+            factor = max(0.0, -1 / self.ramp_interval * (time - self.end_time) + 1.0)
 
         if factor > 0.0:
-            magnetization_vector = self.magnetization_density*system.volume[self.start_idx:self.end_idx]*system.tangents[:,self.start_idx:self.end_idx]
-            magnetic_torques = factor * _batch_cross(magnetization_vector, self.magnetic_field_vector*np.sin(self.wave_number*time))
+            magnetization_vector = (
+                self.magnetization_density
+                * system.volume[self.start_idx : self.end_idx]
+                * system.tangents[:, self.start_idx : self.end_idx]
+            )
+            magnetic_torques = factor * _batch_cross(
+                magnetization_vector,
+                self.magnetic_field_vector * np.sin(self.wave_number * time),
+            )
             np.round_(magnetic_torques, 12, magnetic_torques)
 
-            system.external_torques[...,self.start_idx:self.end_idx] += _batch_matvec(
-                system.director_collection[...,self.start_idx:self.end_idx], magnetic_torques
+            system.external_torques[
+                ..., self.start_idx : self.end_idx
+            ] += _batch_matvec(
+                system.director_collection[..., self.start_idx : self.end_idx],
+                magnetic_torques,
             )
+
 
 """
 I think we should separate the magneticforcing class and thew magnetic field generator itself.
 The magneticforcing can be generic taking in a external_magnetic_field object, which has say attributes
 needed for computing forces and torques (magnetic field gradient and value).
 """
+
+
+class MagneticFieldForcing(NoForces):
+    def __init__(self, external_magnetic_field):
+        # should this be a class member or passed as an argument
+        # to apply_force and apply_torques function?
+        self.external_magnetic_field = external_magnetic_field
+
+    def apply_forces(self, system, time: np.float64 = 0.0):
+        # add a magnetisation \cdot \grad_magnetic_field term here
+        pass
+
+    def apply_torques(self, system, time: np.float64 = 0.0):
+
+        # magnetization_vector will be a (3, n_elem) array and a member of
+        # the MagneticCosseratRod object.
+        # Im guessing Arman knows a better way of doing the statement below
+        element_position_collection = 0.5 * (
+            system.position_collection[..., 1:] + system.position_collection[..., :-1]
+        )
+        local_frame_magnetic_field_value = _batch_matvec(
+            system.director_collection,
+            self.external_magnetic_field.value(position=element_position_collection),
+        )
+        # the above steps can be combined in one line...
+        system.external_torques += _batch_cross(
+            system.magnetization_vector, local_frame_magnetic_field_value
+        )
