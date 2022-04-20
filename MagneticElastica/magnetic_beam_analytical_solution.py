e24029da5f8cbcfab8cb5a2be535a6f8ea2650b7
armantekinalp <armant2@illinois.edu>
2022-04-20 16:13:07 -0500

    black formatting

--- MagneticElastica/magnetic_beam_analytical_solution.py
+++ MagneticElastica/magnetic_beam_analytical_solution.py
@@ -1,31 +1,33 @@
 import numpy as np
 import multiprocessing as mp
 import sys
+
 sys.path.append("../../")
 from scipy.optimize import fsolve, minimize_scalar, minimize
 from scipy.integrate import odeint
 
 
 class MagneticBeamAnalytical:
-    """
+    """ """
 
-    """
-    def __init__(self, F, EI, phi, L, ksi=np.linspace(0,1,100), theta_dot_initial_guess=0.0):
+    def __init__(
+        self, F, EI, phi, L, ksi=np.linspace(0, 1, 100), theta_dot_initial_guess=0.0
+    ):
         self.F = F
         self.EI = EI
         self.phi = phi
 
         self.ksi = ksi
-        self.q = F * L**2 / (EI)
+        self.q = F * L ** 2 / (EI)
         self.theta_dot_initial_guess = theta_dot_initial_guess
 
-    def compute_governing_equations(self, x,  ksi, *args, **kwargs):
+    def compute_governing_equations(self, x, ksi, *args, **kwargs):
 
         x1, x2 = x
 
         x1_dot = x2
 
-        x2_dot = -self.q * np.sin(self.phi - x1 )
+        x2_dot = -self.q * np.sin(self.phi - x1)
 
         return [x1_dot, x2_dot]
 
@@ -40,8 +42,7 @@ class MagneticBeamAnalytical:
         sol = self.solve_governing_equations(x2_0)
 
         # return sol[-1,1]-0
-        return (sol[-1,1]-0)**2
-
+        return (sol[-1, 1] - 0) ** 2
 
     def find_boundary_conditions(self):
         """
@@ -63,35 +64,32 @@ class MagneticBeamAnalytical:
 
         sol = self.solve_governing_equations(x2_0)
 
-        theta = sol[:,0]
+        theta = sol[:, 0]
 
         current_deflection = self._compute_deflection(theta)
 
-
         return theta[-1], current_deflection, x2_0
-
 
     def _compute_deflection(self, theta):
 
         return np.trapz(np.sin(theta), x=self.ksi)
 
 
-
-phi = np.array([30, 60, 90, 120, 150, 180-0.5])
+phi = np.array([30, 60, 90, 120, 150, 180 - 0.5])
 phi = np.deg2rad(phi)
 
 base_length = 6
 base_radius = 0.15
-base_area = np.pi * base_radius**2
-I = np.pi/4 * base_radius**4
-E = 1E6
-M = 144E3
-B = np.linspace(0, 42, 400) * 1E-3
+base_area = np.pi * base_radius ** 2
+I = np.pi / 4 * base_radius ** 4
+E = 1e6
+M = 144e3
+B = np.linspace(0, 42, 400) * 1e-3
 f = M * B * base_area
 
 
-deflection =  np.zeros((phi.shape[0], f.shape[0]))
-theta =  np.zeros((phi.shape[0], f.shape[0]))
+deflection = np.zeros((phi.shape[0], f.shape[0]))
+theta = np.zeros((phi.shape[0], f.shape[0]))
 theta_dot = np.zeros((phi.shape[0], f.shape[0]))
 
 for i in range(phi.shape[0]):
@@ -100,16 +98,22 @@ for i in range(phi.shape[0]):
         # condition at x=L theta_dot = 0. However, optimization methods used can converge to different optima, so
         # we start optimization using the theta_dot computed in previous iteration.
         if j == 0:
-            theta_dot_initial_guess = theta_dot[i,j]
+            theta_dot_initial_guess = theta_dot[i, j]
         else:
-            theta_dot_initial_guess = theta_dot[i,j-1]
+            theta_dot_initial_guess = theta_dot[i, j - 1]
 
-
-        magneto = MagneticBeamAnalytical(F=f[j], EI=E*I, phi=phi[i], L=base_length, ksi=np.linspace(0,1,100), theta_dot_initial_guess=theta_dot_initial_guess)
+        magneto = MagneticBeamAnalytical(
+            F=f[j],
+            EI=E * I,
+            phi=phi[i],
+            L=base_length,
+            ksi=np.linspace(0, 1, 100),
+            theta_dot_initial_guess=theta_dot_initial_guess,
+        )
         temp_theta, temp_deflection, temp_theta_dot = magneto()
-        theta[i,j] = temp_theta
-        deflection[i,j]  = temp_deflection
-        theta_dot[i,j]=temp_theta_dot
+        theta[i, j] = temp_theta
+        deflection[i, j] = temp_deflection
+        theta_dot[i, j] = temp_theta_dot
 
 
 theta = np.rad2deg(theta)
@@ -126,9 +130,9 @@ axs = []
 axs.append(plt.subplot2grid((1, 1), (0, 0)))
 for i in range(phi.shape[0]):
     axs[0].plot(
-    f * base_length**2 / (E*I),
-    deflection[i, :],
-    label="phi=" + str(np.ceil(np.rad2deg(phi[i]))),
+        f * base_length ** 2 / (E * I),
+        deflection[i, :],
+        label="phi=" + str(np.ceil(np.rad2deg(phi[i]))),
     )
 axs[0].set_xlabel("MBAL2/EI", fontsize=20)
 axs[0].set_ylabel("delta_y/L", fontsize=20)
@@ -146,10 +150,10 @@ axs = []
 axs.append(plt.subplot2grid((1, 1), (0, 0)))
 for i in range(phi.shape[0]):
     axs[0].plot(
-        f * base_length**2 / (E*I),
+        f * base_length ** 2 / (E * I),
         theta[i, :],
         label="phi=" + str(np.ceil(np.rad2deg(phi[i]))),
-        )
+    )
 axs[0].set_xlabel("MBAL2/EI", fontsize=20)
 axs[0].set_ylabel("theta(L)", fontsize=20)
 plt.tight_layout()
