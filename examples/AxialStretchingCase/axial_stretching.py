4576fd7d8b767c6840d061391f7c29b6d84f97b1
Bhosale <bhosale2@illinois.edu>
2022-05-17 00:38:26 -0500

    fix: axial stretching-->rescale nu for new scheme

--- examples/AxialStretchingCase/axial_stretching.py
+++ examples/AxialStretchingCase/axial_stretching.py
@@ -57,7 +57,7 @@ base_length = 1.0
 base_radius = 0.025
 base_area = np.pi * base_radius ** 2
 density = 1000
-nu = 2.0
+nu = 1.0
 youngs_modulus = 1e4
 # For shear modulus of 1e4, nu is 99!
 poisson_ratio = 0.5
