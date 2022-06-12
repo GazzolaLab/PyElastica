d3f91f2429f9640fbe707dee1a5bd0f584167bb6
armantekinalp <armant2@illinois.edu>
2022-06-11 22:05:07 -0500

    hotfix:rod-rod contact examples remove poisson ratio from init
    This commit removes poisson ratio from the initialization of rods in
    rod-rod contact examples. Since initializing poisson ratio is
    depreciated.

--- examples/RodContactCase/RodRodContact/rod_rod_contact_inclined_validation.py
+++ examples/RodContactCase/RodRodContact/rod_rod_contact_inclined_validation.py
@@ -30,7 +30,7 @@ base_length = 0.5
 base_radius = 0.01
 base_area = np.pi * base_radius ** 2
 density = 1750
-nu = 0.001
+nu = 0.0
 E = 3e5
 poisson_ratio = 0.5
 shear_modulus = E / (poisson_ratio + 1.0)
@@ -58,7 +58,6 @@ rod_one = CosseratRod.straight_rod(
     density,
     nu,
     E,
-    poisson_ratio,
     shear_modulus=shear_modulus,
 )
 
