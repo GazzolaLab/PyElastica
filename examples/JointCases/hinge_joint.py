474e38c39b3e002dd040b88e5e7ec77f427f8ca3
Bhosale <bhosale2@illinois.edu>
2022-06-16 15:44:41 -0500

    refac: joint cases for new damping style

--- examples/JointCases/hinge_joint.py
+++ examples/JointCases/hinge_joint.py
@@ -18,7 +18,7 @@ from examples.JointCases.joint_cases_postprocessing import (
 
 
 class HingeJointSimulator(
-    BaseSystemCollection, Constraints, Connections, Forcing, CallBacks
+    BaseSystemCollection, Constraints, Connections, Forcing, Damping, CallBacks
 ):
     pass
 
@@ -34,7 +34,6 @@ base_length = 0.2
 base_radius = 0.007
 base_area = np.pi * base_radius ** 2
 density = 1750
-nu = 4e-3
 E = 3e7
 poisson_ratio = 0.5
 shear_modulus = E / (poisson_ratio + 1.0)
@@ -51,7 +50,7 @@ rod1 = CosseratRod.straight_rod(
     base_length,
     base_radius,
     density,
-    nu,
+    0.0,  # internal damping constant, deprecated in v0.3.0
     E,
     shear_modulus=shear_modulus,
 )
@@ -65,7 +64,7 @@ rod2 = CosseratRod.straight_rod(
     base_length,
     base_radius,
     density,
-    nu,
+    0.0,  # internal damping constant, deprecated in v0.3.0
     E,
     shear_modulus=shear_modulus,
 )
@@ -93,6 +92,19 @@ hinge_joint_sim.add_forcing_to(rod2).using(
     normal_direction=normal,
 )
 
+# add damping
+damping_constant = 4e-3
+dt = 1e-5
+hinge_joint_sim.dampen(rod1).using(
+    ExponentialDamper,
+    damping_constant=damping_constant,
+    time_step=dt,
+)
+hinge_joint_sim.dampen(rod2).using(
+    ExponentialDamper,
+    damping_constant=damping_constant,
+    time_step=dt,
+)
 
 # Callback functions
 # Add call backs
