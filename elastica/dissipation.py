a93571aa4b1c28ee151f5008249eb144191eff42
Yashraj Bhosale <yashraj.iitb@gmail.com>
2022-07-05 17:15:40 +0000

    docs: Update elastica/dissipation.py with thesis citation
    Co-authored-by: Arman Tekinalp <53585636+armantekinalp@users.noreply.github.com>

--- elastica/dissipation.py
+++ elastica/dissipation.py
@@ -184,6 +184,9 @@ class FilterDamper(DamperBase):
     Jeanmart, H., & Winckelmans, G. (2007). Investigation of eddy-viscosity
     models modified using discrete filters: a simplified “regularized variational
     multiscale model” and an “enhanced field model”. Physics of fluids, 19(5), 055110.
+    .. [2] Lorieul, G. (2018). Development and validation of a 2D Vortex Particle-Mesh
+     method for incompressible multiphase flows 
+     (Doctoral dissertation, Université Catholique de Louvain).
 
     Attributes
     ----------
