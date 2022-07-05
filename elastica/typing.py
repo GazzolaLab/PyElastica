f35431c7ea3bdbab3bbaa9d045835a9a38b5b064
Yashraj Bhosale <yashraj.iitb@gmail.com>
2022-07-05 16:17:47 +0000

    Update elastica/typing.py
    Co-authored-by: Seung Hyun Kim <skim0119@gmail.com>

--- elastica/typing.py
+++ elastica/typing.py
@@ -4,4 +4,4 @@ from elastica.rigidbody import RigidBodyBase
 from typing import Type, Union
 
 RodType = Type[RodBase]
-SystemType = Union[Type[RodBase], Type[RigidBodyBase]]
+SystemType = Union[RodType, Type[RigidBodyBase]]
