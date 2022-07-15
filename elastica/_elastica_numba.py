af1b7b7fc51f14bd910a1a6b4b5daccba6e22bdf
Bhosale <bhosale2@illinois.edu>
2022-07-15 16:08:51 -0500

    modify elastica_numba deprec message

--- elastica/_elastica_numba.py
+++ elastica/_elastica_numba.py
@@ -1,6 +1,6 @@
 class _elastica_numba:
     """The purpose is to throw deprecation error to people previously
-    using _elastica_numba module. Please remove this in the future.
+    using _elastica_numba module. Please remove this after v0.3.
     """
 
     raise ImportError(
