92835fc9645c32547a376c5f16ef042cd2e7e1a0
Maximilian Stölzle <maximilian@stoelzle.ch>
2022-07-20 16:02:52 +0200

    Add cylinder to `spherical_joint.py` example

--- examples/JointCases/spherical_joint.py
+++ examples/JointCases/spherical_joint.py
@@ -36,8 +36,15 @@ E = 3e7
 poisson_ratio = 0.5
 shear_modulus = E / (poisson_ratio + 1.0)
 
+# setting up time params
+final_time = 10
+dt = 5e-5
+fps = 100  # fps of the video
+step_skip = int(1 / (dt * fps))
+
 start_rod_1 = np.zeros((3,))
 start_rod_2 = start_rod_1 + direction * base_length
+start_cylinder = start_rod_2 + direction * base_length
 
 # Create rod 1
 rod1 = CosseratRod.straight_rod(
@@ -67,6 +74,16 @@ rod2 = CosseratRod.straight_rod(
     shear_modulus=shear_modulus,
 )
 spherical_joint_sim.append(rod2)
+# Create cylinder
+cylinder = Cylinder(
+    start=start_cylinder,
+    direction=direction,
+    normal=normal,
+    base_length=base_length,
+    base_radius=base_radius,
+    density=density,
+)
+spherical_joint_sim.append(cylinder)
 
 # Apply boundary conditions to rod1.
 spherical_joint_sim.constrain(rod1).using(
@@ -79,6 +96,12 @@ spherical_joint_sim.connect(
 ).using(
     FreeJoint, k=1e5, nu=0
 )  # k=kg/s2 nu=kg/s 1e-2
+# Connect rod 2 and cylinder
+spherical_joint_sim.connect(
+    first_rod=rod2, second_rod=cylinder, first_connect_idx=-1, second_connect_idx=0
+).using(
+    FreeJoint, k=1e5, nu=0, point_system_two=np.array([0.0, 0.0, -cylinder.length / 2])
+)
 
 # Add forces to rod2
 spherical_joint_sim.add_forcing_to(rod2).using(
@@ -95,7 +118,6 @@ spherical_joint_sim.add_forcing_to(rod2).using(
 # damping_constant = 4e-3
 # dt = 1e-5
 damping_constant = 4e-3
-dt = 5e-5
 spherical_joint_sim.dampen(rod1).using(
     ExponentialDamper,
     damping_constant=damping_constant,
@@ -109,35 +131,38 @@ spherical_joint_sim.dampen(rod2).using(
 
 pp_list_rod1 = defaultdict(list)
 pp_list_rod2 = defaultdict(list)
+pp_list_cylinder = defaultdict(list)
 
 spherical_joint_sim.collect_diagnostics(rod1).using(
-    MyCallBack, step_skip=1000, callback_params=pp_list_rod1
+    MyCallBack, step_skip=step_skip, callback_params=pp_list_rod1
 )
 spherical_joint_sim.collect_diagnostics(rod2).using(
-    MyCallBack, step_skip=1000, callback_params=pp_list_rod2
+    MyCallBack, step_skip=step_skip, callback_params=pp_list_rod2
+)
+spherical_joint_sim.collect_diagnostics(cylinder).using(
+    MyCallBack, step_skip=step_skip, callback_params=pp_list_cylinder
 )
 
 spherical_joint_sim.finalize()
 timestepper = PositionVerlet()
 # timestepper = PEFRL()
 
-final_time = 10
 dl = base_length / n_elem
 total_steps = int(final_time / dt)
 print("Total steps", total_steps)
 integrate(timestepper, spherical_joint_sim, final_time, total_steps)
 
 PLOT_FIGURE = True
-SAVE_FIGURE = False
+SAVE_FIGURE = True
 PLOT_VIDEO = True
 
 # plotting results
 if PLOT_FIGURE:
     filename = "spherical_joint_example_last_node_pos_xy.png"
     plot_position(
-        pp_list_rod1,
-        pp_list_rod2,
-        plot_params_cylinder=None,
+        plot_params_rod1=pp_list_rod1,
+        plot_params_rod2=pp_list_rod2,
+        plot_params_cylinder=pp_list_cylinder,
         filename=filename,
         SAVE_FIGURE=SAVE_FIGURE,
     )
@@ -145,23 +170,26 @@ if PLOT_FIGURE:
 if PLOT_VIDEO:
     filename = "spherical_joint_example"
     plot_video(
-        pp_list_rod1,
-        pp_list_rod2,
-        plot_params_cylinder=None,
+        plot_params_rod1=pp_list_rod1,
+        plot_params_rod2=pp_list_rod2,
+        plot_params_cylinder=pp_list_cylinder,
+        cylinder=cylinder,
         video_name=filename + ".mp4",
-        fps=100,
+        fps=fps,
     )
     plot_video_xy(
-        pp_list_rod1,
-        pp_list_rod2,
-        plot_params_cylinder=None,
+        plot_params_rod1=pp_list_rod1,
+        plot_params_rod2=pp_list_rod2,
+        plot_params_cylinder=pp_list_cylinder,
+        cylinder=cylinder,
         video_name=filename + "_xy.mp4",
-        fps=100,
+        fps=fps,
     )
     plot_video_xz(
-        pp_list_rod1,
-        pp_list_rod2,
-        plot_params_cylinder=None,
+        plot_params_rod1=pp_list_rod1,
+        plot_params_rod2=pp_list_rod2,
+        plot_params_cylinder=pp_list_cylinder,
+        cylinder=cylinder,
         video_name=filename + "_xz.mp4",
-        fps=100,
+        fps=fps,
     )
