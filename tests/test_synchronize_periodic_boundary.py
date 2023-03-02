e8368135d91015777082946e836fa4caf612ea20
ilianasiriziba <ilianasiriziba@gmail.com>
2023-03-02 13:02:00 -0600

    enhancement: add constrain periodic condition

--- tests/test_synchronize_periodic_boundary.py
+++ tests/test_synchronize_periodic_boundary.py
@@ -5,9 +5,11 @@ from elastica._synchronize_periodic_boundary import (
     _synchronize_periodic_boundary_of_matrix_collection,
     _synchronize_periodic_boundary_of_vector_collection,
     _synchronize_periodic_boundary_of_scalar_collection,
+    _ConstrainPeriodicBoundaries,
 )
 from elastica.utils import Tolerance
 import pytest
+from tests.test_rod.test_rods import MockTestRingRod
 
 
 @pytest.mark.parametrize("n_elems", [10, 30, 40])
@@ -113,6 +115,73 @@ def test_synchronize_periodic_boundary_scalar(n_elems):
     _synchronize_periodic_boundary_of_scalar_collection(input_matrix, periodic_idx)
 
     assert_allclose(correct_matrix, input_matrix, atol=Tolerance.atol())
+
+
+def test_ConstrainPeriodicBoundaries():
+    n_elem = 29
+    test_rod = MockTestRingRod()
+    fixed_rod = _ConstrainPeriodicBoundaries(_system=test_rod)
+    test_position_collection = np.random.rand(3, n_elem + 3)
+    test_rod.position_collection = (
+        test_position_collection.copy()
+    )  # We need copy of the list not a reference to this array
+    test_director_collection = np.random.rand(3, 3, n_elem + 2)
+    test_rod.director_collection = (
+        test_director_collection.copy()
+    )  # We need copy of the list not a reference to this array
+    fixed_rod.constrain_values(test_rod, time=0)
+
+    periodic_boundary_node_idx = np.array([[0, n_elem + 1, n_elem + 2], [n_elem, 1, 2]])
+    periodic_boundary_elems_idx = np.array([[0, n_elem + 1], [n_elem, 1]])
+
+    for i in range(3):
+        for k in range(periodic_boundary_node_idx.shape[1]):
+            test_position_collection[
+                i, periodic_boundary_node_idx[0, k]
+            ] = test_position_collection[i, periodic_boundary_node_idx[1, k]]
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(periodic_boundary_elems_idx.shape[1]):
+                test_director_collection[
+                    i, j, periodic_boundary_elems_idx[0, k]
+                ] = test_director_collection[i, j, periodic_boundary_elems_idx[1, k]]
+
+    assert_allclose(
+        test_position_collection, test_rod.position_collection, atol=Tolerance.atol()
+    )
+    assert_allclose(
+        test_director_collection, test_rod.director_collection, atol=Tolerance.atol()
+    )
+
+    test_velocity_collection = np.random.rand(3, n_elem + 3)
+    test_rod.velocity_collection = (
+        test_velocity_collection.copy()
+    )  # We need copy of the list not a reference to this array
+    test_omega_collection = np.random.rand(3, n_elem + 2)
+    test_rod.omega_collection = (
+        test_omega_collection.copy()
+    )  # We need copy of the list not a reference to this array
+    fixed_rod.constrain_rates(test_rod, time=0)
+
+    for i in range(3):
+        for k in range(periodic_boundary_node_idx.shape[1]):
+            test_velocity_collection[
+                i, periodic_boundary_node_idx[0, k]
+            ] = test_velocity_collection[i, periodic_boundary_node_idx[1, k]]
+
+    for i in range(3):
+        for k in range(periodic_boundary_elems_idx.shape[1]):
+            test_omega_collection[
+                i, periodic_boundary_elems_idx[0, k]
+            ] = test_omega_collection[i, periodic_boundary_elems_idx[1, k]]
+
+    assert_allclose(
+        test_velocity_collection, test_rod.velocity_collection, atol=Tolerance.atol()
+    )
+    assert_allclose(
+        test_omega_collection, test_rod.omega_collection, atol=Tolerance.atol()
+    )
 
 
 if __name__ == "__main__":
