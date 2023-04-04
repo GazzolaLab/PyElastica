72013103f9e8ce6731765ea1735067079b3a9f38
AsadNizami <asadnizami123@gmail.com>
2023-04-04 22:25:50 +0530

    update

--- elastica/wrappers.py
+++ elastica/wrappers.py
@@ -1,13 +1,6 @@
 import warnings
 
 
-from elastica.modules.base_system import BaseSystemCollection
-from elastica.modules.connections import Connections
-from elastica.modules.constraints import Constraints
-from elastica.modules.forcing import Forcing
-from elastica.modules.callbacks import CallBacks
-from elastica.modules.damping import Damping
-
 warnings.warn(
     "elastica.wrappers is refactored to elastica.modules in version 0.3.0.",
     DeprecationWarning,
