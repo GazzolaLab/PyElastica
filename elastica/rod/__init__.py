d943f1df84da066706138902fe1b009668c2af02
AsadNizami <asadnizami123@gmail.com>
2023-04-06 00:41:02 +0530

    removed all wildcards import

--- elastica/rod/__init__.py
+++ elastica/rod/__init__.py
@@ -1,6 +1,6 @@
 __doc__ = """Rod classes and its data structures """
 
 
-from elastica.rod.knot_theory import *
+from elastica.rod.knot_theory import KnotTheory
 from elastica.rod.data_structures import *
 from elastica.rod.rod_base import RodBase
