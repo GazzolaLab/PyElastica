53f1109e973b0baa85945a13c36210faca2a8404
Rahul-JOON <rahuljoon16@gmail.com>
2023-06-30 00:13:46 +0530

    Mesh faces attribute and testing

--- mesh/mesh_initializer.py
+++ mesh/mesh_initializer.py
@@ -1,6 +1,7 @@
 __doc__ = """ Mesh Initializer using Pyvista """
 
 import pyvista as pv
+import numpy as np
 
 
 class MeshInitialize():
@@ -17,3 +18,23 @@ class MeshInitialize():
     """
     def __init__(self, filepath):
         self.mesh = pv.read(filepath)
+        self.pyvista_faces = self.mesh.faces
+        self.number_of_faces = self.mesh.n_faces
+        self.pyvista_points = self.mesh.points
+        self.faces = self.face_calculation(self.pyvista_faces, self.pyvista_points, self.number_of_faces)
+
+    def face_calculation(self, pvfaces, meshpoints, n_faces):
+        """
+        This function converts the faces from pyvista to pyelastica geometry
+        """
+
+        faces = np.zeros((3, 3, n_faces))
+        vertice_no = 0
+
+        for i in range(n_faces):
+            vertice_no += 1
+            for j in range(3):
+                faces[..., j, i] = meshpoints[pvfaces[vertice_no]]
+                vertice_no += 1
+
+        return faces
---
+++ tests/test_mesh_init.py
@@ -0,0 +1,43 @@
+__doc__ = """ Test mesh initialization in Elastica """
+
+from mesh.mesh_initializer import MeshInitialize
+import numpy as np
+from numpy.testing import assert_allclose
+
+
+"""
+A dummy cube mesh stl file is used for testing.
+This dummy file was created using the open source code for creating meshes using 'numpy-stl'
+in numpy-stl documentation (https://numpy-stl.readthedocs.io/en/latest/usage.html#initial-usage)
+"""
+
+
+def test_mesh_faces():
+
+    """
+    This function initializes a new cube mesh checks for the geometry
+    of faces generated.
+    """
+
+    mockmesh = MeshInitialize(r"assets\cube.stl")
+    calculated_faces = np.array(
+        [
+            [
+                [-1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, -1],
+                [-1, -1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1],
+                [1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1],
+            ],
+            [
+                [-1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1],
+                [1, 1, -1, 1, -1, 1, -1, 1, 1, 1, -1, -1],
+                [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1],
+            ],
+            [
+                [-1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1],
+                [-1, -1, 1, 1, 1, 1, -1, -1, -1, 1, -1, 1],
+                [-1, -1, 1, -1, 1, 1, -1, 1, 1, 1, 1, 1],
+            ],
+        ]
+    )
+
+    assert_allclose(mockmesh.faces, calculated_faces)
