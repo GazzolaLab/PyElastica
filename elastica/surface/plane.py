802907f000be21a119233cbd0ade32a495d0b1d7
Ali-7800 <47090295+Ali-7800@users.noreply.github.com>
2023-08-21 17:52:14 -0500

    Surface subclass: plane, and tests

---
+++ elastica/surface/plane.py
@@ -0,0 +1,32 @@
+__doc__ = """plane surface class"""
+
+from elastica.surface.surface_base import SurfaceBase
+import numpy as np
+from numpy.testing import assert_allclose
+from elastica.utils import Tolerance
+
+
+class Plane(SurfaceBase):
+    def __init__(self, plane_origin, plane_normal):
+        """
+        Plane surface initializer.
+
+        Parameters
+        ----------
+        plane_origin: numpy.ndarray
+        2D (dim, 1) array containing data with 'float' type.
+        Origin of the plane.
+        plane_normal: numpy.ndarray
+        2D (dim, 1) array containing data with 'float' type.
+        The normal vector of the plane, must be normalized.
+        """
+
+        assert_allclose(
+            np.linalg.norm(plane_normal),
+            1,
+            atol=Tolerance.atol(),
+            err_msg="plane normal is not a unit vector",
+        )
+        self.normal = plane_normal.reshape(3)
+        self.origin = plane_origin.reshape(3, 1)
+        self.tol = 1e-4
---
+++ tests/test_surface/test_plane.py
@@ -0,0 +1,38 @@
+__doc__ = """Tests for plane surface class"""
+import numpy as np
+from numpy.testing import assert_allclose
+from elastica.utils import Tolerance
+from elastica.surface import Plane
+
+
+# tests Initialisation of plane
+def test_plane_initialization():
+    """
+    This test case is for testing initialization of rigid sphere and it checks the
+    validity of the members of sphere class.
+
+    Returns
+    -------
+
+    """
+    # setting up test params
+    plane_origin = np.random.rand(3).reshape(3, 1)
+    plane_normal_direction = np.random.rand(3).reshape(3)
+    plane_normal = plane_normal_direction / np.linalg.norm(plane_normal_direction)
+
+    test_plane = Plane(plane_origin, plane_normal)
+    # checking plane origin
+    assert_allclose(
+        test_plane.origin,
+        plane_origin,
+        atol=Tolerance.atol(),
+    )
+
+    # checking plane normal
+    assert_allclose(test_plane.normal, plane_normal, atol=Tolerance.atol())
+
+
+if __name__ == "__main__":
+    from pytest import main
+
+    main([__file__])
