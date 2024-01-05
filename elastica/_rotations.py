1e6859447ef6015cc9f1867b373af9e8f5443138
Noel Naughton <noel.mnaughton@gmail.com>
2024-01-05 11:09:29 -0500

    Update elastica/_rotations.py
    Co-authored-by: Seung Hyun Kim <skim0119@gmail.com>

--- elastica/_rotations.py
+++ elastica/_rotations.py
@@ -149,7 +149,7 @@ def _inv_rotate(director_collection):
         # Clip the trace to between -3 and 3.
         # Any deviation beyond this is numerical error
         trace = min(trace, 3.0)
-        trace = max(trace, -3.0)
+        trace = max(trace, -1.0)
         theta = arccos(0.5 * trace - 0.5)
 
         # TODO HARDCODED bugfix has to be changed. Remove 1e-14 tolerance
