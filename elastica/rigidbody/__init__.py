3c7bc5aad47d326fdfe8b45e7b6d4d7f11e56968
Songyuan Cui <cui20@illinois.edu>
2024-05-07 16:02:46 -0500

    chore: internal _typing.py for typedefs for rebase

--- elastica/rigidbody/__init__.py
+++ elastica/rigidbody/__init__.py
@@ -2,3 +2,5 @@ from .rigid_body import RigidBodyBase
 from .cylinder import Cylinder
 from .sphere import Sphere
 from .mesh_rigid_body import MeshRigidBody
+
+from ._typing import float_t, f_arr_t, int_t
