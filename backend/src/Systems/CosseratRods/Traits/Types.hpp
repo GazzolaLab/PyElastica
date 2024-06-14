b3221cd2c49e24014db8ec59d051c0a63a2726c3
Seung Hyun Kim <skim449@illinois.edu>
2024-06-14 18:46:04 +0900

    add blaze traits

---
+++ backend/src/Systems/CosseratRods/Traits/Types.hpp
@@ -0,0 +1,35 @@
+//==============================================================================
+/*!
+//  \file
+//  \brief All trait types belonging to a Cosserat rod
+//
+//  Copyright (C) 2020-2020 Tejaswin Parthasarathy - All Rights Reserved
+//  Copyright (C) 2020-2020 MattiaLab - All Rights Reserved
+//
+//  Distributed under the MIT License.
+//  See LICENSE.txt for details.
+*/
+//==============================================================================
+
+#pragma once
+
+namespace elastica {
+
+  namespace cosserat_rod {
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // Forward declarations of cosserat rod trait types
+    //
+    ////////////////////////////////////////////////////////////////////////////
+
+    //************************************************************************
+    /*! \cond ELASTICA_INTERNAL */
+    struct DataOpsTraits;
+    // struct PlacementTrait;
+    /*! \endcond */
+    //************************************************************************
+
+  }  // namespace cosserat_rod
+
+}  // namespace elastica
