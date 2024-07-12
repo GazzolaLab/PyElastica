946459a95ef3b0ac2a64fdd2669c1cc3292e0eeb
Seung Hyun Kim <skim0119@gmail.com>
2024-07-12 00:05:09 -0500

    files: add Materials

---
+++ backend/src/Simulator/Materials.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+
+// Forward declarations come first
+#include "Simulator/Materials/Types.hpp"
+//
+#include "Simulator/Materials/Materials.hpp"
+// any pre-registered materials (like biological etc.)
