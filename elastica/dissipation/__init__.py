2a50fb5d748d37668347ce68a62b85874793ee2f
Songyuan Cui <cui20@illinois.edu>
2024-11-26 14:46:10 -0600

    refactor: remove disspation directory

--- elastica/dissipation/__init__.py
+++
@@ -1,9 +0,0 @@
-__doc__ = """
-(added in version 0.3.3)
-
-Built-in damper module implementations
-"""
-
-from .damper_base import DamperBase
-from .analytical_linear_damper import AnalyticalLinearDamper
-from .laplacian_dissipation_filter import LaplaceDissipationFilter
