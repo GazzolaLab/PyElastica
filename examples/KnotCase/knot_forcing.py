Updated parameters to smoothen motion, fixed shear modulus calculation, moved branching to apply_forces

examples/KnotCase/knot_forcing.py
@@ -137,28 +137,28 @@ class SnapForce(NoForces):
         for i in range(len(self.snap_time)):
             if (self.snap_time[i] == 0) and (np.linalg.norm(self.snap_targets[i] - system.position_collection[..., self.snap_nodes[i]]) < self.distance):
                 self.snap_time[i] = time
-        self.compute_end_point_forces(
-            system.external_forces,
-            self.force_mag,
-            time,
-            self.ramp_up_time,
-            system.position_collection,
-            self.snap_nodes,
-            self.snap_targets,
-            self.snap_time
-        )
+            if (self.snap_time[i] != 0):
+                self.compute_end_point_forces(
+                    system.external_forces,
+                    self.force_mag,
+                    time,
+                    self.ramp_up_time,
+                    system.position_collection[..., self.snap_nodes[i]],
+                    self.snap_nodes[i],
+                    self.snap_targets[i],
+                    self.snap_time[i]
+                )
         
 
     @staticmethod
     @njit(cache=True)
     def compute_end_point_forces(
-        external_forces, force_mag, time, ramp_up_time, position_collection, snap_nodes, snap_targets,last_snap_time
+        external_forces, force_mag, time, ramp_up_time, snap_node_position, snap_node, snap_target,snap_time
     ):
-        for i in range(len(snap_nodes)):
-            if (last_snap_time[i] != 0):
-                factor = min(1.0, (time - last_snap_time[i]) / ramp_up_time)
-                snap_force = force_mag * (snap_targets[i] - position_collection[..., snap_nodes[i]]) / np.linalg.norm(snap_targets[i] - position_collection[..., snap_nodes[i]])
-                external_forces[..., snap_nodes[i]] += snap_force * factor
+        factor = min(1.0, ((time - snap_time) / ramp_up_time)**2)
+        distance_factor = min(np.linalg.norm(snap_target - snap_node_position), 1) * 0.2 + 0.8
+        snap_force = force_mag * (snap_target - snap_node_position) / np.linalg.norm(snap_target - snap_node_position)
+        external_forces[..., snap_node] += snap_force * factor * distance_factor
             
 
 

examples/KnotCase/knot_sim.py
@@ -22,21 +22,21 @@ GENERATE_2D_VIDEO = True
 GENERATE_3D_VIDEO = True
 
 stretch_sim = StretchingBeamSimulator()
-final_time = 60
+final_time = 25
 dt = 0.0005
 
 # setting up test params
-n_elem = 50
+n_elem = 80
 start = np.zeros((3,))
 direction = np.array([1.0, 0.0, 0.0])
 normal = np.array([0.0, 1.0, 0.0])
-base_length = 1.5
+base_length = 1.2
 base_radius = 0.025
 base_area = np.pi * base_radius ** 2
 density = 1000
 youngs_modulus =  2e3 
-poisson_ratio = 0.5
-shear_modulus = youngs_modulus / (poisson_ratio + 1.0)
+poisson_ratio = 0.1
+shear_modulus = youngs_modulus / (2 * (poisson_ratio + 1.0))
 
 stretchable_rod = ea.CosseratRod.straight_rod(
     n_elem,
@@ -56,30 +56,29 @@ stretch_sim.constrain(stretchable_rod).using(ea.FixedConstraint, constrained_pos
 
 #Endpoint target forces
 force_mag = 1.0
-targets = np.array([[.85,0.03,.85],
-                    [0.0,0.06,1.2],
-                    [-0.60,0.09,.70],
-                    [-0.4,0.12,-0.4],
-                    [0.1,0.15,-0.7],
-                    [0.15,-0.5,-0.5],
-                    [0.2,-0.3,0.1],
-                    [0.2,0.1,0.12],
-                    [0.2,0.3,0.12],
-                    [0.0,5.0,0.0]])  # Target positions for the end node
+targets = np.array([[.68,0.03,.68],
+                    [0.0,0.06,0.95],
+                    [-0.45,0.09,.55],
+                    [-0.3,0.12,-0.35],
+                    [0.1,0.09,-0.55],
+                    [0.15,-0.3,-0.45],
+                    [0.2,-0.1,0.04],
+                    [0.2,0.1,0.08],
+                    [0.2,0.3,0.08],
+                    [0.0,5.0,0.0]])   # Target positions for the end node
 
 stretch_sim.add_forcing_to(stretchable_rod).using(MultiTargetForce, force_mag,ramp_up_time=1,targets=targets)
 
 #Snap forces - certain nodes along the rod are pulled to intermediate targets once in range to aid in forming the knot
-snap_force_mag = 2.0
-snap_targets = np.array([[0.27,0.0,0.0],
-                         [0.3,0.05,0.2],
-                         [0.2,0.1,-0.05]])
-snap_nodes = np.array([10,17,33])
+snap_force_mag = 0.5
+snap_targets = np.array([[0.27,0.0,0.03],
+                         [0.0,0.1,0.05]])
+snap_nodes = np.array([22,42])
 
 stretch_sim.add_forcing_to(stretchable_rod).using(SnapForce, snap_force_mag, snap_nodes, snap_targets, ramp_up_time=3, distance=.05, stop_target = targets[-3], stop_distance = 0.04)
 
 #Damping
-damping_constant = .4
+damping_constant = 0.2
 stretch_sim.dampen(stretchable_rod).using(
     ea.AnalyticalLinearDamper,
     damping_constant=damping_constant,
@@ -88,7 +87,7 @@ stretch_sim.dampen(stretchable_rod).using(
 
 stretch_sim.dampen(stretchable_rod).using(
     ea.LaplaceDissipationFilter,
-    filter_order=4
+    filter_order=3
 )
 
 #Self contact