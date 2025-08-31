62284bb3845f8ea89717198a06261c40f413e742
Seung Hyun Kim <skim0119@gmail.com>
2025-08-31 03:16:10 -0500

    remove deprecated file

--- elastica/memory_block/memory_block_rod_base.py
+++
@@ -1,8 +0,0 @@
-__doc__ = """Deprecated module. Use memory_blocks.utils instead."""
-import numpy as np
-import numpy.typing as npt
-
-from .utils import (
-    make_block_memory_metadata,
-    make_block_memory_periodic_boundary_metadata,
-)
